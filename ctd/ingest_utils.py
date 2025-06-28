8a827759a8f33052f12daa7a4eb46985215c9850
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2025-06-27 20:23:11 -0700

    initial import

---
+++ ctd/ingest_utils.py
@@ -0,0 +1,89 @@
+import biolink_model
+import os
+import pandas
+import uuid
+from typing import Callable, TypeVar, Type, Optional, TextIO, Iterable, Any, Union
+from pydantic import BaseModel
+
+DataFrame = pandas.DataFrame
+
+
+
+from biolink_model.datamodel.pydanticmodel_v2 import (NamedThing, Association)
+
+
+def make_fix_comment_in_column_name(comment_char: str) -> Callable:
+    def _fix_comment_in_column_name(df: pandas.DataFrame):
+        df.columns = [df.columns[0].lstrip(comment_char).lstrip(' ')] + list(df.columns[1:])
+        return df
+    return _fix_comment_in_column_name
+
+
+def make_tx_col_func(col: str,
+                     f: Callable) -> Callable:
+    def _tx_col_func(df: DataFrame) -> DataFrame:
+        df[col] = df[col].apply(f)
+        return df
+    return _tx_col_func
+
+
+def make_mutator_filter(col: str,
+                        value: Union[str, int, float, bool]):
+    def _mutator_filter(df: DataFrame):
+        return df[df[col] == value]
+    return _mutator_filter
+
+
+T = TypeVar('T', bound=NamedThing)
+def make_nodes(df: pandas.DataFrame,
+                id_col: str,
+                name_col: str,
+                cls: type[T]) -> tuple[T]:
+    return tuple(
+        cls(**row)
+        for row in (
+            df[[id_col, name_col]]
+            .dropna()
+            .drop_duplicates()
+            .rename(columns={id_col: "id", name_col: "name"})
+            .to_dict(orient="records")
+        )
+    )
+
+
+A = TypeVar('A', bound=Association)
+def make_assocs(df: pandas.DataFrame,
+                subject_id_col: str,
+                object_id_col: str,
+                pmids_col: str,
+                fixed_properties: dict,
+                column_fixers: tuple[Callable, ...],
+                cls: type[A]) -> tuple[A]:
+    fixed_df = df[[subject_id_col,
+                   object_id_col,
+                   pmids_col]].dropna().drop_duplicates()    
+    if column_fixers:
+        for f in column_fixers:
+            fixed_df = f(fixed_df)
+    return tuple(
+        cls(**(row |
+               {'id': str(uuid.uuid4())} |
+               fixed_properties))
+        for row in (
+                (
+                fixed_df
+                .rename(columns={subject_id_col: "subject",
+                                 object_id_col: "object",
+                                 pmids_col: "publications"})
+                .to_dict(orient="records")) 
+        )
+    )
+
+
+def save_to_jsonl(iter_serializable: Iterable[BaseModel],
+                  file_name: str):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    with open(file_name, 'a') as fo:
+        for m in iter_serializable:
+            print(m.model_dump_json(), file=fo)
---
+++ ctd/transform_ctd.py
@@ -0,0 +1,49 @@
+import pandas
+import biolink_model
+import ingest_utils as iu
+from typing import Callable, TypeVar, Type, Optional
+
+CHUNK_SIZE = 100_000
+
+
+from biolink_model.datamodel.pydanticmodel_v2 import (ChemicalEntity,
+                                                      Disease,
+                                                      ChemicalToDiseaseOrPhenotypicFeatureAssociation,
+                                                      KnowledgeLevelEnum,
+                                                      AgentTypeEnum)
+
+BIOLINK_TREATS_OR_APPLIED_OR_STUDIED_TO_TREAT = "biolink:treats_or_applied_or_studied_to_treat"
+INFORES_CTD = 'infores:ctd'
+
+# C-style typedefs here
+DataFrame = pandas.DataFrame
+TextFileReader = pandas.io.parsers.readers.TextFileReader
+
+
+ctd_df = (pandas.read_csv("https://ctdbase.org/reports/CTD_chemicals_diseases.tsv.gz",
+                          header=27,
+                          delimiter="\t")
+          .pipe(iu.make_fix_comment_in_column_name('#'))
+          .pipe(iu.make_mutator_filter("DirectEvidence", "therapeutic")))
+
+nodes = sum(map(lambda args: iu.make_nodes(ctd_df, *args),
+                (("ChemicalID", "ChemicalName", ChemicalEntity),
+                 ("DiseaseID", "DiseaseName", Disease))), ())
+
+iu.save_to_jsonl(nodes,
+                 'ctd-output-nodes.jsonl')
+
+associations = iu.make_assocs(ctd_df,
+                              "ChemicalID",
+                              "DiseaseID",
+                              "PubMedIDs",
+                              {'predicate': BIOLINK_TREATS_OR_APPLIED_OR_STUDIED_TO_TREAT,
+                               'primary_knowledge_source': INFORES_CTD,
+                               'knowledge_level': KnowledgeLevelEnum.knowledge_assertion,
+                               'agent_type': AgentTypeEnum.manual_agent},
+                              (iu.make_tx_col_func("PubMedIDs",
+                                                   lambda p: tuple(p.split("|"))),),
+                              ChemicalToDiseaseOrPhenotypicFeatureAssociation)
+
+iu.save_to_jsonl(associations,
+                 'ctd-output-edges.jsonl')
