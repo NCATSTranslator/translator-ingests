bef12d9224257b8d3a82f0bf47cb89a225be45f1
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-03 14:04:46 -0700

    Interim commit of additional roughed in data set module code

--- src/translator_ingest/util/storage/dataset.py
+++ src/translator_ingest/util/storage/dataset.py
@@ -1,10 +1,15 @@
 # Abstract encapsulation of Translator Ingest process datasets
 # Probably will be a collection of KGX jsonlines + metadata files
 # but for now, we specify the concept in an agnostic fashion here
-from typing import Optional
+from enum import Enum
+from typing import Optional, Dict
 from os import urandom
 from uuid import UUID, uuid4
 from ..kgx import KGX
+
+class DataSetType(Enum):
+    PKS = "Primary Knowledge Source Knowledge Graph format"
+    KGX = "Knowledge Graph eXchange ('KGX') Format"
 
 class DataSetId(UUID):
     def __init__(self):
@@ -12,17 +17,26 @@ class DataSetId(UUID):
         # TODO: can I make a variant with UUID input argument?
         UUID.__init__(self, bytes=urandom(16), version=4)
 
+class NodeId(str):
+    pass
+
+class EdgeId(UUID):
+    def __init__(self):
+        # emulates uuid4() via the UUID constructor
+        # TODO: can I make a variant with UUID input argument?
+        UUID.__init__(self, bytes=urandom(16), version=4)
 
 # TODO: this class is just a stub proxy for some real data, so very incomplete!
 class DataSet:
 
-    def __init__(self, dataset_id: Optional[DataSetId] = None):
+    def __init__(self, dataset_id: Optional[DataSetId] = None, dataset_type: DataSetType = KGX):
         """
         Constructor for initialization of an empty dataset. Issues a new DataSetId for the DataSet
         if an existing DataSetId is not provided in as an argument to the construction.
         """
         # A dataset_id may be provided if the dataset already exists somewhere...
         self._dataset_id = dataset_id if dataset_id is not None else DataSetId()
+        self.dataset_type = dataset_type
 
         # ... however, we defer initialization of the KGX dataset to a later time
         self._kgx: Optional[KGX] = None
@@ -42,8 +56,44 @@ class DataSet:
     def get_curie(self) -> str:
         """
         Gets the CURIE string format of the DataSet identifier for data documentation purposes.
-        however, most of the software within the project  (e.g. storage access methods) will generally want to use
+        However, most of the software within the project  (e.g. storage access methods) will generally want to use
         the get_id() method to use the direct internal Python representation of the dataset identifier.
         :return: CURIE string representation of the DataSet identifier
         """
         return self._dataset_id.urn
+
+    def publish_node(self, data: Dict) -> Optional[NodeId]:
+        """
+        The contents of a node are defined internally as a
+        multi-level Python dictionary representing a JSON object
+        with properties defined by the type of dataset.
+        :param data:
+        :return: NodeId of the newly created node in the DataSet
+        """
+        if self.validate_node(data):
+            # TODO: process node here
+            return None  # NodeId()
+        else:
+            return None
+
+    def publish_edge(self, data: Dict) -> Optional[EdgeId]:
+        """
+        The contents of an edge are defined internally as a
+        multi-level Python dictionary representing a JSON object
+        with properties defined by the type of dataset.
+        :param data:
+        :return: EdgeId of the newly created edge in the DataSet
+        """
+        if self.validate_edge(data):
+            # TODO: process edge here
+            return None  # EdgeId()
+        else:
+            return None  # EdgeId()
+
+    def validate_node(self, data: Dict) -> bool:
+        # TODO: Dataset type-specific validation of a node
+        return False
+
+    def validate_edge(self, data: Dict) -> bool:
+        # TODO: Dataset type-specific validation of a node
+        return False
