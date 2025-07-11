0fcc7e45567a3240fb9236e76419ef15a54e41a4
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-11 16:39:48 -0700

    Clone core Koza code, converted one copy to a comment (to guide early development & troubleshooting of the new implementation... This code ought to disappear later...

--- src/translator_ingest/ingests/hpoa/gene_to_disease_transform.py
+++ src/translator_ingest/ingests/hpoa/gene_to_disease_transform.py
@@ -26,6 +26,37 @@ def prepare(records: Iterator[Dict] = None) -> Iterator[Dict] | None:
     return records
 """
 
+#
+##### ORIGINAL Koza-centric ingest code
+#
+# koza_app = get_koza_app("hpoa_gene_to_disease")
+#
+#
+# while (row := koza_app.get_row()) is not None:
+#     gene_id = row["ncbi_gene_id"]
+#     disease_id = row["disease_id"].replace("ORPHA:", "Orphanet:")
+#
+#     predicate = get_predicate(row["association_type"])
+#     primary_knowledge_source, aggregator_knowledge_source = get_knowledge_sources(row["source"],
+#                                                                                   INFORES_MONARCHINITIATIVE)
+#
+#     if predicate == BIOLINK_CAUSES:
+#         association_class = CausalGeneToDiseaseAssociation
+#     else:
+#         association_class = CorrelatedGeneToDiseaseAssociation
+#
+#     association = association_class(id="uuid:" + str(uuid.uuid1()),
+#                                     subject=gene_id,
+#                                     predicate=predicate,
+#                                     object=disease_id,
+#                                     primary_knowledge_source=primary_knowledge_source,
+#                                     aggregator_knowledge_source=aggregator_knowledge_source,
+#                                     knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+#                                     agent_type=AgentTypeEnum.manual_agent)
+#
+#     koza_app.write(association)
+#
+
 
 koza_app = get_koza_app("hpoa_gene_to_disease")
 
@@ -35,7 +66,7 @@ while (row := koza_app.get_row()) is not None:
     disease_id = row["disease_id"].replace("ORPHA:", "Orphanet:")
 
     predicate = get_predicate(row["association_type"])
-    primary_knowledge_source, aggregator_knowledge_source = get_knowledge_sources(row["source"], 
+    primary_knowledge_source, aggregator_knowledge_source = get_knowledge_sources(row["source"],
                                                                                   INFORES_MONARCHINITIATIVE)
 
     if predicate == BIOLINK_CAUSES:
@@ -53,7 +84,6 @@ while (row := koza_app.get_row()) is not None:
                                     agent_type=AgentTypeEnum.manual_agent)
 
     koza_app.write(association)
-
 
 """
 this is just an example of the interface, using transform() offers the opportunity to do something more efficient
--- src/translator_ingest/ingests/hpoa/gene_to_phenotype_transform.py
+++ src/translator_ingest/ingests/hpoa/gene_to_phenotype_transform.py
@@ -29,11 +29,57 @@ def prepare(records: Iterator[Dict] = None) -> Iterator[Dict] | None:
 # slot we need to update the association we make
 # https://github.com/biolink/biolink-model/pull/1524
 
+#
+##### ORIGINAL Koza-centric ingest code
+#
+#
+# # Initiate koza app and mondo map from sssom file
+# koza_app = get_koza_app("hpoa_gene_to_phenotype")
+# mondo_map = koza_app.get_map('mondo_map')
+#
+# while (row := koza_app.get_row()) is not None:
+#     gene_id = "NCBIGene:" + row["ncbi_gene_id"]
+#     phenotype_id = row["hpo_id"]
+#
+#     # No frequency data provided
+#     if row["frequency"] == "-":
+#         frequency = Frequency()
+#     else:
+#         # Raw frequencies - HPO term curies, ratios, percentages - normalized to HPO terms
+#         frequency: Frequency = phenotype_frequency_to_hpo_term(row["frequency"])
+#
+#     # Convert to mondo id if possible, otherwise leave as is
+#     org_id = row["disease_id"].replace("ORPHA:", "Orphanet:")
+#     dis_id = org_id
+#     if dis_id in mondo_map:
+#         dis_id = mondo_map[dis_id]['subject_id']
+#
+#
+#     publications = [pub.strip() for pub in row["publications"].split(";")] if row["publications"] else []
+#
+#     association = GeneToPhenotypicFeatureAssociation(id="uuid:" + str(uuid.uuid1()),
+#                                                      subject=gene_id,
+#                                                      predicate="biolink:has_phenotype",
+#                                                      object=phenotype_id,
+#                                                      aggregator_knowledge_source=["infores:monarchinitiative"],
+#                                                      primary_knowledge_source="infores:hpo-annotations",
+#                                                      knowledge_level=KnowledgeLevelEnum.logical_entailment,
+#                                                      agent_type=AgentTypeEnum.automated_agent,
+#                                                      frequency_qualifier=frequency.frequency_qualifier if frequency.frequency_qualifier else None,
+#                                                      has_percentage=frequency.has_percentage,
+#                                                      has_quotient=frequency.has_quotient,
+#                                                      has_count=frequency.has_count,
+#                                                      has_total=frequency.has_total,
+#                                                      disease_context_qualifier=dis_id,
+#                                                      publications=publications)
+#
+#     koza_app.write(association)
+#
+
 
 # Initiate koza app and mondo map from sssom file
 koza_app = get_koza_app("hpoa_gene_to_phenotype")
 mondo_map = koza_app.get_map('mondo_map')
-
 
 while (row := koza_app.get_row()) is not None:
     gene_id = "NCBIGene:" + row["ncbi_gene_id"]
@@ -45,13 +91,12 @@ while (row := koza_app.get_row()) is not None:
     else:
         # Raw frequencies - HPO term curies, ratios, percentages - normalized to HPO terms
         frequency: Frequency = phenotype_frequency_to_hpo_term(row["frequency"])
-    
+
     # Convert to mondo id if possible, otherwise leave as is
     org_id = row["disease_id"].replace("ORPHA:", "Orphanet:")
     dis_id = org_id
     if dis_id in mondo_map:
         dis_id = mondo_map[dis_id]['subject_id']
-
 
     publications = [pub.strip() for pub in row["publications"].split(";")] if row["publications"] else []
 
