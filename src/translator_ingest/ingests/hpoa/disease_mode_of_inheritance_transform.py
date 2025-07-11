204b73447b697a7c151587c22c67f0eeed7e4abd
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-11 16:39:48 -0700

    Clone core Koza code, converted one copy to a comment (to guide early development & troubleshooting of the new implementation... This code ought to disappear later...

--- src/translator_ingest/ingests/hpoa/disease_mode_of_inheritance_transform.py
+++ src/translator_ingest/ingests/hpoa/disease_mode_of_inheritance_transform.py
@@ -54,6 +54,58 @@ def prepare(records: Iterator[Dict] = None) -> Iterator[Dict] | None:
 
 # Read hpo mode of inheritance terms into memory using pronto + hp.obo file + HP:0000005 (Mode of Inheritance)
 modes_of_inheritance = read_ontology_to_exclusion_terms("data/hp.obo", umbrella_term="HP:0000005", include=True)
+
+#
+##### ORIGINAL Koza-centric ingest code
+#
+# koza_app = get_koza_app("hpoa_disease_mode_of_inheritance")
+#
+# while (row := koza_app.get_row()) is not None:
+#
+#     # Object: Actually a Genetic Inheritance (as should be specified by a suitable HPO term)
+#     # TODO: perhaps load the proper (Genetic Inheritance) node concepts into the Monarch Graph (simply as Ontology terms?).
+#     hpo_id = row["hpo_id"]
+#
+#     # We ignore records that don't map to a known HPO term for Genetic Inheritance
+#     # (as recorded in the locally bound 'hpoa-modes-of-inheritance' table)
+#     if hpo_id and hpo_id in modes_of_inheritance:
+#
+#         # Nodes
+#
+#         # Subject: Disease
+#         disease_id = row["database_id"]
+#
+#         # Predicate (canonical direction)
+#         predicate = "biolink:has_mode_of_inheritance"
+#
+#         # Annotations
+#
+#         # Three letter ECO code to ECO class based on HPO documentation
+#         evidence_curie = evidence_to_eco[row["evidence"]]
+#
+#         # Publications
+#         publications_field: str = row["reference"]
+#         publications: List[str] = publications_field.split(";")
+#
+#         # Filter out some weird NCBI web endpoints
+#         publications = [p for p in publications if not p.startswith("http")]
+#
+#         # Association/Edge
+#         association = DiseaseOrPhenotypicFeatureToGeneticInheritanceAssociation(id="uuid:" + str(uuid.uuid1()),
+#                                                                                 subject=disease_id,
+#                                                                                 predicate=predicate,
+#                                                                                 object=hpo_id,
+#                                                                                 publications=publications,
+#                                                                                 has_evidence=[evidence_curie],
+#                                                                                 aggregator_knowledge_source=["infores:monarchinitiative"],
+#                                                                                 primary_knowledge_source="infores:hpo-annotations",
+#                                                                                 knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+#                                                                                 agent_type=AgentTypeEnum.manual_agent)
+#         koza_app.write(association)
+#
+#     else:
+#         logger.warning(f"HPOA ID field value '{str(hpo_id)}' is missing or an invalid disease mode of inheritance?")
+
 koza_app = get_koza_app("hpoa_disease_mode_of_inheritance")
 
 while (row := koza_app.get_row()) is not None:
