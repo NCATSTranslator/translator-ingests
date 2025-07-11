edf452edf39d8bcfd437f846214abd09253be499
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-11 16:43:23 -0700

    CTD ingest code copied into files as a comment, to help with design of the ingest submodule... This code ought to disappear later...

--- src/translator_ingest/ingests/hpoa/gene_to_disease_transform.py
+++ src/translator_ingest/ingests/hpoa/gene_to_disease_transform.py
@@ -57,6 +57,25 @@ def prepare(records: Iterator[Dict] = None) -> Iterator[Dict] | None:
 #     koza_app.write(association)
 #
 
+#
+## Sample template record parser (from CTD); used as a guide for coding (to be delated later)
+#
+# def transform_record(record: Dict) -> (Iterator[Entity], Iterator[Association]):
+#     chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
+#     disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
+#     association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
+#         id=str(uuid.uuid4()),
+#         subject=chemical.id,
+#         predicate=BIOLINK_TREATS_OR_APPLIED_OR_STUDIED_TO_TREAT,
+#         object=disease.id,
+#         publications=["PMID:" + p for p in record["PubMedIDs"].split("|")],
+#         # is this code/repo an aggregator in this context? feels like no, but maybe yes?
+#         # aggregator_knowledge_source=["infores:???"],
+#         primary_knowledge_source=INFORES_CTD,
+#         knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+#         agent_type=AgentTypeEnum.manual_agent,
+#     )
+#     return [chemical, disease], [association]
 
 koza_app = get_koza_app("hpoa_gene_to_disease")
 
