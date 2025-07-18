05439a66cd9c2c86d82c42bf85bd632592822bd1
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-17 17:33:39 -0700

    Cleaned up some tech debt

--- src/translator_ingest/ingests/hpoa/phenotype_ingest_utils.py
+++ src/translator_ingest/ingests/hpoa/phenotype_ingest_utils.py
@@ -198,25 +198,25 @@ def get_predicate(original_predicate: str) -> str:
         raise ValueError(f"Unknown predicate: {original_predicate}")
 
 
-# General function to read an .obo ontology file into memory using pronto to gather all terms that do not fall under a particular parent class
+# General function to read an .obo ontology file into memory
+# using pronto to gather all terms that do not fall under a particular parent class
 def read_ontology_to_exclusion_terms(ontology_obo_file, umbrella_term="HP:0000118", include=False):
     
-    # Read ontology file into memory
+    # Read the ontology file into memory
     onto = Ontology(ontology_obo_file)
     exclude_terms = {}
     term_count = len(list(onto.terms()))
     
     for term in onto.terms():
         
-        # Gather ancestor terms and update our filtering datastructure accordingly
+        # Gather ancestor terms and update our filtering data structure accordingly
         parent_terms = {ancestor.id: ancestor.name for ancestor in term.superclasses()}
-        if include == False:
+        if not include:
             if umbrella_term not in parent_terms:
                 exclude_terms.update({term.id:term.name})
 
-        elif include == True:
-            if umbrella_term in parent_terms:
-                exclude_terms.update({term.id:term.name})
+        elif umbrella_term in parent_terms:
+            exclude_terms.update({term.id:term.name})
     
     print("- Terms from ontology found that do not belong to parent class {} {}/{}".format(umbrella_term,
                                                                                            format(len(exclude_terms)), 
