0d144b5c479499a36a7c7d29c61c78df033f3340
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-07-23 13:00:42 -0700

    Replace versioning code with Mock

--- src/translator_ingest/ingests/hpoa/__init__.py
+++ src/translator_ingest/ingests/hpoa/__init__.py
@@ -1,14 +1,24 @@
 """translator_ingest.ingests.hpoa package."""
-import importlib_metadata
+"""
+Stub implementation of a wrapper for the 
+Human Phenotype Ontology Annotation (HPOA) as a DataSource,
+just to allow for temporary mock implementation of
+expected Translator Ingest pipeline features, like version capture.
+Should generally be replaced by a shared code implementation
+(currently a design work-in-progress within DINGO)
+"""
+from typing import Optional
 
-try:
-    __version__ = importlib_metadata.version(__name__)
-except importlib_metadata.PackageNotFoundError:
-    # package is not installed
-    __version__ = "0.0.0"  # pragma: no cover
+class HPOADataSource:
+    """
+    MVP Implementation of an HPOA DataSource wrapper
+    """
+    @classmethod
+    def get_version(cls) -> str:
+        # Hardcoded mock implementation, HPOA release, published as of July 23, 2025
+        # TODO: replace with dynamically discovery of data source version
+        #       as will be implemented by shared code in system (T.B.A.)
+        return "2025-05-06"
 
-# TODO: this "shared" versioning of HPOA is probably totally wrong
-#       in that this versioning metric is a migrant relic from the
-#       monarch-phenotype-profile-ingest Git repository package
-def get_latest_version():
-    return __version__
+def get_latest_version() -> Optional[str]:
+    return HPOADataSource.get_version()
