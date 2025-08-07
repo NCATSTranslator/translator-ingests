ce156e49a6cd376fcb41bc5e48f32dff564c3476
Evan Morris <evandietzmorris@gmail.com>
2025-08-07 14:06:46 -0400

    implementing multiple readers
    # Conflicts:
    #	src/translator_ingest/ingests/ctd/ctd.py

--- src/translator_ingest/ingests/ctd/ctd.py
+++ src/translator_ingest/ingests/ctd/ctd.py
@@ -8,10 +8,9 @@ from biolink_model.datamodel.pydanticmodel_v2 import (
     ChemicalEntity,
     ChemicalToDiseaseOrPhenotypicFeatureAssociation,
     Disease,
-    NamedThing,
     KnowledgeLevelEnum,
     AgentTypeEnum,
-    Association
+    Entity
 )
 from bs4 import BeautifulSoup
 
@@ -32,23 +31,26 @@ def get_latest_version():
     else:
         raise RuntimeError('Could not determine latest version for CTD, "pgheading" header was missing...')
 
-"""
-Functions decorated with @koza.on_data_begin() run before transform or transform_record
 
-koza.state is a dictionary that can be used to store arbitrary variables
-@koza.on_data_begin()
-def prepare(koza: koza.KozaTransform) -> None:
-    koza.state['example_counter'] = 1
+#Functions decorated with @koza.on_data_begin() run before transform or transform_record
 
-Functions decorated with @koza.on_data_end() run after transform or transform_record
-@koza.on_data_end()
-def clean_up(koza: koza.KozaTransform) -> None:
-    if koza.state['example_counter'] > 0:
-        koza.log(f'Uh oh, {koza.state['example_counter']} things happened!', level="WARNING")
-"""
+#koza.state is a dictionary that can be used to store arbitrary variables
+@koza.on_data_begin(tag="chemical_to_disease")
+def on_begin_chemical_to_disease(koza: koza.KozaTransform) -> None:
+    koza.state['example_error_counter'] = 1
+    koza.log(f'On Data Begin... chemical_to_disease', level="INFO")
 
-@koza.transform_record()
-def transform_record(koza: koza.KozaTransform, record: dict[str, Any]) -> tuple[Iterable[NamedThing], Iterable[Association]]:
+
+#Functions decorated with @koza.on_data_end() run after transform or transform_record
+@koza.on_data_end(tag="chemical_to_disease")
+def on_end_chemical_to_disease(koza: koza.KozaTransform) -> None:
+    koza.log(f'On Data End... chemical_to_disease', level="INFO")
+    if koza.state['example_error_counter'] > 1:
+        koza.log(f'Uh oh, {koza.state['example_error_counter']} things happened!', level="WARNING")
+
+
+@koza.transform_record(tag="chemical_to_disease")
+def transform_record_chemical_to_disease(koza: koza.KozaTransform, record: dict[str, Any]) -> Iterable[Entity] | None:
     chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
     disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
     association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
@@ -63,4 +65,39 @@ def transform_record(koza: koza.KozaTransform, record: dict[str, Any]) -> tuple[
         knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
         agent_type=AgentTypeEnum.manual_agent,
     )
-    return [chemical, disease], [association]
+    return [chemical, disease, association]
+
+
+
+@koza.on_data_begin(tag="exposure_events")
+def on_begin_exposure_events(koza: koza.KozaTransform) -> None:
+    koza.log(f'On Data Begin... exposure_events', level="INFO")
+    koza.state['missing_predicate'] = 0
+    koza.state['missing_disease'] = 0
+    koza.state['all_predicates_labels'] = set()
+
+
+#Functions decorated with @koza.on_data_end() run after transform or transform_record
+@koza.on_data_end(tag="exposure_events")
+def on_end_exposure_events(koza: koza.KozaTransform) -> None:
+    koza.log(f'On Data End.. exposure_events', level="INFO")
+    koza.log(f'all CTD predicate values: {koza.state['all_predicates_labels']}', level="INFO")
+
+
+@koza.transform_record(tag="exposure_events")
+def transform_record_exposure_events(koza: koza.KozaTransform, record: dict[str, Any]) -> Iterable[Entity] | None:
+    disease_id = f'MESH:{record['diseaseid']}'
+    if not disease_id:
+        koza.state['missing_disease'] += 1
+
+    predicate_label = record['outcomerelationship']
+    if not predicate_label:
+        koza.state['missing_predicate'] += 1
+
+    koza.state['all_predicates_labels'].add(predicate_label)
+    return None
+
+    exposure_id = f'MESH:{record['exposurestressorid']}'
+    publications = f'PMID:{record['reference']}'
+
+    return None
