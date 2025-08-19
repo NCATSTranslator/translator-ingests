1d7e2beb450c1738842a9579dbde85886139d768
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-08-19 10:24:19 -0700

    Initial copying over of Monarch-hosted AGRKB ingest code, over to Translator Ingest pipeline, for conversion

---
+++ src/translator_ingest/ingests/__init__.py
@@ -0,0 +1,10 @@
+"""
+translator_ingest.ingests.agrkb package.
+
+Shared utilities for the Alliance Genomic Resources ingest:
+
+    get_version() - get version from ???
+"""
+
+def get_latest_version() -> str:
+    return "<AGRKB release>"
