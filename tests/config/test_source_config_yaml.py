9ec36b136857c17c90f60195c34380ff0b908bab
Evan Morris <evandietzmorris@gmail.com>
2025-09-04 16:42:25 -0400

    making tests autodetect ingests

--- tests/config/test_source_config_yaml.py
+++ tests/config/test_source_config_yaml.py
@@ -1,21 +1,21 @@
 import pytest
+# import warnings
 import yaml
 
 from koza import KozaConfig
 from koza.io.yaml_loader import UniqueIncludeLoader
 
-from ..util import get_ingest_config_yaml_path
+from ..util import get_ingest_config_yaml_path, ALL_SOURCE_IDS
 
 
-@pytest.mark.parametrize("source_id",
-[
-    "ctd",
-    "go_cam",
-    "goa"
-])
+@pytest.mark.parametrize("source_id", ALL_SOURCE_IDS)
 def test_valid_source_config_yaml(source_id):
     # get the path to a specific source config yaml file
     config_yaml_file_path = get_ingest_config_yaml_path(source_id)
+    if config_yaml_file_path is None:
+        # warnings.warn(f"An ingest directory exists for {source_id} but a config yaml was not found.", UserWarning)
+        return
+
     # this is how koza opens and parses the yaml
     with config_yaml_file_path.open("r") as fh:
         config_dict = yaml.load(fh, Loader=UniqueIncludeLoader.with_file_base(str(config_yaml_file_path)))  # noqa: S506
--- tests/integration/test_koza_runner.py
+++ tests/integration/test_koza_runner.py
@@ -1,20 +1,21 @@
 import pytest
+# import warnings
 
 from koza.runner import KozaRunner
 
-from ..util import get_ingest_config_yaml_path
+from ..util import get_ingest_config_yaml_path, ALL_SOURCE_IDS
 
 
-@pytest.mark.parametrize("source_id", [
-    "ctd",
-    "go_cam",
-    "goa"
-])
+@pytest.mark.parametrize("source_id", ALL_SOURCE_IDS)
 def test_koza_runner_init_from_config(source_id, tmp_path):
     # get the path to a specific source config yaml file
     config_yaml_file_path = get_ingest_config_yaml_path(source_id)
+    if config_yaml_file_path is None:
+        # warnings.warn(f"An ingest directory exists for {source_id} but a config yaml was not found.", UserWarning)
+        return
+
     # initialize a KozaRunner from the config file
-    config, runner = KozaRunner.from_config_file(config_yaml_file_path, output_dir=str(tmp_path))
+    config, runner = KozaRunner.from_config_file(str(config_yaml_file_path), output_dir=str(tmp_path))
     # ensure at least one transform function was identified for every tag
     for tag, hooks in runner.hooks_by_tag.items():
         if not (hooks and (hooks.transform or hooks.transform_record)):
