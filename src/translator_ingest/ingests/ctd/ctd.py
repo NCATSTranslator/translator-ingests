3b17bb580c01bac5627bc3e6801379423ac8eabc
Evan Morris <evandietzmorris@gmail.com>
2025-09-10 17:04:41 -0400

    completing CTD for all edge types, commenting out exposures

--- src/translator_ingest/ingests/ctd/ctd.py
+++ src/translator_ingest/ingests/ctd/ctd.py
@@ -32,72 +32,78 @@ def get_latest_version():
     else:
         raise RuntimeError('Could not determine latest version for CTD, "pgheading" header was missing...')
 
-
-#Functions decorated with @koza.on_data_begin() run before transform or transform_record
-
-#koza.state is a dictionary that can be used to store arbitrary variables
 @koza.on_data_begin(tag="chemical_to_disease")
 def on_begin_chemical_to_disease(koza: koza.KozaTransform) -> None:
-    koza.state['example_error_counter'] = 1
-    koza.log('On Data Begin... chemical_to_disease', level="INFO")
+    koza.state["rows_missing_publications"] = {}
+    koza.state["rows_missing_publications"]["therapeutic"] = 0
+    koza.state["rows_missing_publications"]["marker_mechanism"] = 0
+    koza.state["rows_missing_publications"]["inference"] = 0
 
-
-#Functions decorated with @koza.on_data_end() run after transform or transform_record
 @koza.on_data_end(tag="chemical_to_disease")
 def on_end_chemical_to_disease(koza: koza.KozaTransform) -> None:
-    koza.log('On Data End... chemical_to_disease', level="INFO")
-    if koza.state['example_error_counter'] > 1:
-        koza.log(f'Uh oh, {koza.state['example_error_counter']} things happened!', level="WARNING")
+    for row_type, count in koza.state['rows_missing_publications'].items():
+        koza.log(f"CTD chemical_to_disease: {count} {row_type} rows with 0 publications", level="WARNING")
 
+CTD_PREDICATES_BY_EVIDENCE_TYPE = {
+    "therapeutic": "biolink:treats_or_applied_or_studied_to_treat",
+    "marker/mechanism": "biolink:correlated_with",
+    "inference": "biolink:associated_with"  # the files don't have "inference" but we use it in the transform
+}
 
 @koza.transform_record(tag="chemical_to_disease")
 def transform_record_chemical_to_disease(koza: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
-    chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
+    chemical = ChemicalEntity(id=f"MESH:{record["ChemicalID"]}", name=record["ChemicalName"])
     disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
+
+    # check the evidence type and assign a predicate based on that
+    evidence_type = record["DirectEvidence"] if record["DirectEvidence"] else "inference"
+    predicate = CTD_PREDICATES_BY_EVIDENCE_TYPE[evidence_type]
+
     publications = [f"PMID:{p}" for p in record["PubMedIDs"].split("|")] if record["PubMedIDs"] else None
+    if not publications:
+        koza.state["rows_missing_publications"][evidence_type] += 1
+
     association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
         id=entity_id(),
         subject=chemical.id,
-        predicate="biolink:treats_or_applied_or_studied_to_treat",
+        predicate=predicate,
         object=disease.id,
         publications=publications,
-        # is this code/repo an aggregator in this context? feels like no, but maybe yes?
-        # aggregator_knowledge_source=["infores:???"],
         sources=build_association_knowledge_sources(primary=INFORES_CTD),
         knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
         agent_type=AgentTypeEnum.manual_agent,
     )
+    if evidence_type == "inference":
+        association.has_confidence_score = float(record["InferenceScore"])
     return KnowledgeGraph(nodes=[chemical, disease], edges=[association])
 
 
-
-@koza.on_data_begin(tag="exposure_events")
-def on_begin_exposure_events(koza: koza.KozaTransform) -> None:
-    koza.log('On Data Begin... exposure_events', level="INFO")
-    koza.state['missing_predicate'] = 0
-    koza.state['missing_disease'] = 0
-    koza.state['all_predicates_labels'] = set()
-
-
-#Functions decorated with @koza.on_data_end() run after transform or transform_record
-@koza.on_data_end(tag="exposure_events")
-def on_end_exposure_events(koza: koza.KozaTransform) -> None:
-    koza.log('On Data End.. exposure_events', level="INFO")
-    koza.log(f'all CTD predicate values: {koza.state['all_predicates_labels']}', level="INFO")
-
-
-@koza.transform_record(tag="exposure_events")
-def transform_record_exposure_events(koza: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
-    disease_id = f'MESH:{record['diseaseid']}'
-    if not disease_id:
-        koza.state['missing_disease'] += 1
-
-    predicate_label = record['outcomerelationship']
-    if not predicate_label:
-        koza.state['missing_predicate'] += 1
-
-    koza.state['all_predicates_labels'].add(predicate_label)
-    return None
-
-    # exposure_id = f'MESH:{record['exposurestressorid']}'
-    # publications = f'PMID:{record['reference']}'
+# @koza.on_data_begin(tag="exposure_events")
+# def on_begin_exposure_events(koza: koza.KozaTransform) -> None:
+#     koza.log('On Data Begin... exposure_events', level="INFO")
+#     koza.state['missing_predicate'] = 0
+#     koza.state['missing_disease'] = 0
+#     koza.state['all_predicates_labels'] = set()
+#
+#
+# Functions decorated with @koza.on_data_end() run after transform or transform_record
+# @koza.on_data_end(tag="exposure_events")
+# def on_end_exposure_events(koza: koza.KozaTransform) -> None:
+#     koza.log('On Data End.. exposure_events', level="INFO")
+#     koza.log(f'all CTD predicate values: {koza.state['all_predicates_labels']}', level="INFO")
+#
+#
+# @koza.transform_record(tag="exposure_events")
+# def transform_record_exposure_events(koza: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
+#     disease_id = f'MESH:{record['diseaseid']}'
+#     if not disease_id:
+#         koza.state['missing_disease'] += 1
+#
+#     predicate_label = record['outcomerelationship']
+#     if not predicate_label:
+#         koza.state['missing_predicate'] += 1
+#
+#     koza.state['all_predicates_labels'].add(predicate_label)
+#
+#     exposure_id = f'MESH:{record['exposurestressorid']}'
+#     publications = f'PMID:{record['reference']}'
