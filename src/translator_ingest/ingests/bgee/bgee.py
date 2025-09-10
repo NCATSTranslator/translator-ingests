13e143e2d307afb78986c3300b68b6dd129d2079
Daniel Korn <To>
2025-09-10 12:56:13 -0400

    Changed ingest template for BGee file structure.

--- src/translator_ingest/ingests/bgee/bgee.py
+++ src/translator_ingest/ingests/bgee/bgee.py
@@ -4,13 +4,16 @@ import pandas as pd
 from typing import Any, Iterable
 
 from biolink_model.datamodel.pydanticmodel_v2 import (
-    ChemicalEntity,
-    ChemicalToDiseaseOrPhenotypicFeatureAssociation,
-    Disease,
-    NamedThing,
+    GeneToExpressionSiteAssociation,
+    Gene,
+    Cell,
+#    ChemicalToDiseaseOrPhenotypicFeatureAssociation,
+#    Disease,
+#    NamedThing,
     KnowledgeLevelEnum,
     AgentTypeEnum,
-    Association
+    Attribute
+#    Association
 )
 from koza.model.graphs import KnowledgeGraph
 
@@ -28,8 +31,8 @@ from koza.model.graphs import KnowledgeGraph
 
 # Biolink predicates and infores identifiers will eventually be imported from automatically updated modules,
 # but for now use hardcoded constants.
-BIOLINK_RELATED_TO = "biolink:related_to"
-INFORES_CTD = "infores:ctd"
+BIOLINK_EXPRESSED_IN = "biolink:expressed_in"
+INFORES_BGEE = "infores:bgee"
 
 # Always implement a function that returns a string representing the latest version of the source data.
 # Ideally, this is the version provided by the knowledge source, directly associated with a specific data download.
@@ -52,35 +55,6 @@ def on_end_ingest_by_record(koza: koza.KozaTransform) -> None:
     if koza.state['example_counter'] > 0:
         koza.log(f'{koza.state['example_counter']} rows were discarded for having no publications.', level="INFO")
 
-# Functions decorated with @koza.prepare_data() are optional. They are called after on_data_begin but before transform.
-# They take an Iterable of dictionaries, typically representing the rows of a source data file, and return an Iterable
-# of dictionaries which will be the data passed to subsequent transform functions. This allows for operations like
-# nontrivial merging or transforming of complex source data on a source wide level, even if the transform will occur
-# with a per record transform function.
-@koza.prepare_data(tag="ingest_by_record")
-def prepare(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]] | None:
-
-    # do pandas stuff
-    df = pd.DataFrame(data)
-    return df.dropna().drop_duplicates().to_dict(orient="records")
-
-    # do database stuff:
-    # import sqlite3
-    # con = sqlite3.connect("example.db")
-    # con.row_factory = sqlite3.Row
-    # cur = con.cursor()
-    # cur.execute("SELECT * FROM example_table")
-    # records = cursor.fetchall()
-    # for record in records:
-    #     yield record
-    # con.close()
-
-    # merge stuff in a custom way
-    # koza.state['nodes'] = defaultdict(dict)
-    # for record in data:
-    #    koza.state['nodes'][record['node_id']] # store some merged node properties or something like that
-
-
 # Ingests must implement a function decorated with @koza.transform() OR @koza.transform_record() (not both).
 # These functions should contain the core data transformation logic generating and returning KnowledgeGraph objects
 # with NamedThing (nodes) and Association (edges) from source data.
@@ -89,65 +63,32 @@ def prepare(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterabl
 # row in a source data file, and returns a KnowledgeGraph with any number of nodes and/or edges.
 @koza.transform_record(tag="ingest_by_record")
 def transform_ingest_by_record(koza: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
-
-    # here is an example of skipping a record based off of some condition
-    publications = [f"PMID:{p}" for p in record["PubMedIDs"].split("|")] if record["PubMedIDs"] else None
-    if not publications:
-        koza.state['example_counter'] += 1
-        return None
-
-    chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-    disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-    association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
+#    attribute_list = [
+#        Attribute(id=str(uuid.uuid4()),has_attribute_type="NCIT:C64214",name="BGee_CallQuality",has_qualitative_value=record["Call quality"]),
+#        Attribute(id=str(uuid.uuid4()),name="BGee_FDR",has_quantitative_value=record["FDR"]),
+#        Attribute(id=str(uuid.uuid4()),name="BGee_ExpressionScore",has_quantitative_value=record["Expression score"]),
+#        Attribute(id=str(uuid.uuid4()),name="BGee_ExpressionRank",has_quantitative_value=record["Expression rank"])
+#    ]
+    attribute_dict = {
+        "BGee_CallQuality":record["Call quality"],
+        "BGee_FDR":record["FDR"],
+        "BGee_ExpressionScore":record["Expression score"],
+        "BGee_ExpressionRank":record["Expression rank"],
+    }
+    #The attribute for each record must be formatted as a list of string, not sure
+    # if this is expected behavior.
+    attribute_list = [str(attribute_dict)]
+    gene = Gene(id="ENSEMBLE:" + record["Gene ID"], name=record["Gene name"])
+    cell = Cell(id=record["Anatomical entity ID"], name=record["Anatomical entity name"])
+    association = GeneToExpressionSiteAssociation(
         id=str(uuid.uuid4()),
-        subject=chemical.id,
-        predicate=BIOLINK_RELATED_TO,
-        object=disease.id,
-        publications=publications,
-        primary_knowledge_source=INFORES_CTD,
-        knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-        agent_type=AgentTypeEnum.manual_agent,
+        subject=gene.id,
+        predicate=BIOLINK_EXPRESSED_IN,
+        object=cell.id,
+        primary_knowledge_source=INFORES_BGEE,
+        knowledge_level=KnowledgeLevelEnum.knowledge_assertion,        
+        agent_type=AgentTypeEnum.not_provided,
+        adjusted_p_value=record["FDR"],
+        has_attribute=attribute_list,
     )
-    return KnowledgeGraph(nodes=[chemical, disease], edges=[association])
-
-# As an alternative to transform_record, functions decorated with @koza.transform() take a KozaTransform and an Iterable
-# of dictionaries, typically corresponding to all the rows in a source data file, and return an iterable of
-# KnowledgeGraph, each containing any number of nodes and/or edges. Any number of KnowledgeGraphs can be returned:
-# all at once, in batches, or using a generator for streaming.
-@koza.transform(tag="ingest_all")
-def transform_ingest_all(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
-    nodes: list[NamedThing] = []
-    edges: list[Association] = []
-    for record in data:
-        chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-        disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-        association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-            id=str(uuid.uuid4()),
-            subject=chemical.id,
-            predicate=BIOLINK_RELATED_TO,
-            object=disease.id,
-            primary_knowledge_source=INFORES_CTD,
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.manual_agent,
-        )
-        nodes.append(chemical)
-        nodes.append(disease)
-        edges.append(association)
-    return [KnowledgeGraph(nodes=nodes, edges=edges)]
-
-# Here is an example using a generator to stream results
-@koza.transform(tag="ingest_all_streaming")
-def transform_ingest_all_streaming(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
-    for record in data:
-        chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-        disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-        association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-            id=str(uuid.uuid4()),
-            subject=chemical.id,
-            predicate=BIOLINK_RELATED_TO,
-            object=disease.id,
-            primary_knowledge_source=INFORES_CTD,
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.manual_agent,
-        )
-        yield KnowledgeGraph(nodes=[chemical, disease], edges=[association])
+    return KnowledgeGraph(nodes=[gene, cell], edges=[association])
