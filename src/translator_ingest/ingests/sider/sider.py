6d869e62c8833f6c59bf93214acc91a3904a9a00
vdancik <vdancik@broadinstitute.org>
2025-09-24 15:12:36 -0400

    test for SIDER ingest

--- src/translator_ingest/ingests/sider/sider.py
+++ src/translator_ingest/ingests/sider/sider.py
@@ -1,24 +1,18 @@
 import uuid
 import koza
-import pandas as pd
 from typing import Any, Iterable
 
 import json
-import os
 import re
 
 from biolink_model.datamodel.pydanticmodel_v2 import (
     ChemicalEntity,
     ChemicalToDiseaseOrPhenotypicFeatureAssociation,
-    Disease,
-    NamedThing,
+    DiseaseOrPhenotypicFeature,
     KnowledgeLevelEnum,
     AgentTypeEnum,
-    Association
 )
 from translator_ingest.util.biolink import (
-    INFORES_CTD,
-    entity_id,
     build_association_knowledge_sources
 )
 from koza.model.graphs import KnowledgeGraph
@@ -52,45 +46,38 @@ for t in transformations:
     t.re = re.compile(t.regex_pattern)
 
 
-
-# !!! README First !!!
-#
-# This module provides a template with example code and instructions for implementing an ingest. Replace the body
-# of function examples below with ingest specific code and delete all template comments or unused functions.
-#
-# Note about ingest tags: for the ingests with multiple different input files and/or different transformation processes,
-# ingests can be divided into multiple sections using tags. Examples from this template are "ingest_by_record",
-# "ingest_all", and "transform_ingest_all_streaming". Tags should be declared as keys in the readers section of ingest
-# yaml files, then included with the (tag="tag_id") syntax as parameters in corresponding koza decorators.
-
-
-# Always implement a function that returns a string representing the latest version of the source data.
-# Ideally, this is the version provided by the knowledge source, directly associated with a specific data download.
-# If a source does not implement versioning, we need to do it. For static datasets, assign a version string
-# corresponding to the current version. For sources that are updated regularly, use file modification dates if
-# possible, or the current date. Versions should (ideally) be sortable (ie YYYY-MM-DD) and should contain no spaces.
+# A function that returns a string representing the latest version of the source data.
 def get_latest_version() -> str:
     return latest_version
 
 
-
+# Implement a function that returns an iterable of dicts, each dict representing a row of the source data.
+# The ingest framework will call this function once per transform.
 @koza.transform(tag="sider_se_reader")
 def transform_ingest_all_streaming(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
     for record in data:
+        # apply transformations
         for t in transformations:
             column_name = getattr(column, t.column)
             m = t.re.match(record[column_name])
             if m:
                 record[column_name] = t.replacement.format(*m.groups())
+        # create nodes and edges
         chemical = ChemicalEntity(id=curie_prefix.CID + record[column.CID_stereo])
-        disease = Disease(id=curie_prefix.UMLS+record[column.UMLS_id], name=record[column.side_effect_name])
+        disease = DiseaseOrPhenotypicFeature(id=curie_prefix.UMLS+record[column.UMLS_id], name=record[column.side_effect_name])
         association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
             id=str(uuid.uuid4()),
             subject=chemical.id,
             predicate=predicate,
             object=disease.id,
+            sources = build_association_knowledge_sources(infores),                
             primary_knowledge_source=infores,
             knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
             agent_type=AgentTypeEnum.manual_agent,
         )
-        yield KnowledgeGraph(nodes=[chemical, disease], edges=[association])
+        knowledgeGraph = KnowledgeGraph(nodes=[chemical, disease], edges=[association])
+        # filter to only PT terms
+        if record[column.MedDRA_concept_type] != 'PT':
+            knowledgeGraph = None
+        # return results
+        yield knowledgeGraph
