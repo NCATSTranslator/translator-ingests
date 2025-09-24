9b1177286f17688b1c16622215c5b4e2a2363bbd
vdancik <vdancik@broadinstitute.org>
2025-09-24 15:12:36 -0400

    test for SIDER ingest

--- tests/unit/ingests/sider/test_sider_ingest.py
+++ tests/unit/ingests/sider/test_sider_ingest.py
@@ -8,9 +8,8 @@ import koza
 from koza.transform import  Mappings
 from koza.io.writer.writer import KozaWriter
 
-from translator_ingest.ingests._ingest_template._ingest_template import (
-    on_begin_ingest_by_record,
-    transform_ingest_by_record
+from translator_ingest.ingests.sider.sider import (
+    transform_ingest_all_streaming
 )
 
 from tests.unit.ingests import validate_transform_result, MockKozaWriter, MockKozaTransform
@@ -36,9 +35,7 @@ ASSOCIATION_TEST_SLOTS = [
     "category",
     "subject",
     "predicate",
-    "negated",
     "object",
-    "publications",
     "sources",
     "knowledge_level",
     "agent_type"
@@ -48,62 +45,54 @@ ASSOCIATION_TEST_SLOTS = [
 @pytest.mark.parametrize(
     "test_record,result_nodes,result_edge",
     [
-        (  # Query 0 - Missing PubMedIDs - returns None
-            {
-                "ChemicalName": "10074-G5",
-                "ChemicalID": "C534883",
-                "CasRN": "",
-                "DiseaseName": "Burkitt Lymphoma",
-                "DiseaseID": "MESH:D002051",
-                "DirectEvidence": "",
-                "InferenceGeneSymbol": "MYC",
-                "InferenceScore": "",
-                "OmimIDs": "113970",
-                "PubMedIDs": "",  # empty expected field, hence, parse doesn't return a knowledge graph
-            },
+        (  # Query 0 - concept_type = LLT -> returns None
+            [{
+                "label": "EMA/WC500020092.html",
+                "STITCH_compound_id_flat": "CID100216416",
+                "STITCH_compound_id_stereo": "CID000216416",
+                "UMLS_concept_id_label": "C0000737",
+                "MedDRA_concept_type": "LLT",
+                "UMLS_concept_id": "C0000737",
+                "side_effect_name": "Abdominal pain",
+            }],
             None,
             None
         ),
         (  # Query 1 - Another record complete with PubMedIDs
-            {
+            [{
 
-                "ChemicalName": "10074-G5",
-                "ChemicalID": "C534883",
-                "CasRN": "",
-                "DiseaseName": "Androgen-Insensitivity Syndrome",
-                "DiseaseID": "MESH:D013734",
-                "DirectEvidence": "",
-                "InferenceGeneSymbol": "AR",
-                "InferenceScore": "6.89",
-                "OmimIDs": "300068|312300",
-                "PubMedIDs": "1303262|8281139",
-            },
+                "label": "safety/2008_-_May_PI_-_Viread_PI.html",
+                "STITCH_compound_id_flat": "CID100119830",
+                "STITCH_compound_id_stereo": "CID005481350",
+                "UMLS_concept_id_label": "C1608945",
+                "MedDRA_concept_type": "PT",
+                "UMLS_concept_id": "C1608945",
+                "side_effect_name": "Exfoliative rash",
+            }],
 
             # Captured node contents
             [
                 {
-                    "id": "MESH:C534883",
-                    "name": "10074-G5",
+                    "id": "PUBCHEM.COMPOUND:5481350",
                     "category": ["biolink:ChemicalEntity"]
                 },
                 {
-                    "id": "MESH:D013734",
-                    "name": "Androgen-Insensitivity Syndrome",
-                    "category": ["biolink:Disease"]
+                    "id": "UMLS:C1608945",
+                    "name": "Exfoliative rash",
+                    "category": ["biolink:DiseaseOrPhenotypicFeature"]
                 }
             ],
 
             # Captured edge contents
             {
                 "category": ["biolink:ChemicalToDiseaseOrPhenotypicFeatureAssociation"],
-                "subject": "MESH:C534883",
-                "predicate": "biolink:related_to",
-                "object": "MESH:D013734",
-                "publications": ["PMID:1303262", "PMID:8281139"],
+                "subject": "PUBCHEM.COMPOUND:5481350",
+                "predicate": "biolink:has_side_effect",
+                "object": "UMLS:C1608945",
                 "sources": [
                     {
                         "resource_role": "primary_knowledge_source",
-                        "resource_id": "infores:ctd"
+                        "resource_id": "infores:sider"
                     }
                 ],
 
@@ -119,11 +108,12 @@ def test_ingest_transform(
         result_nodes: Optional[list],
         result_edge: Optional[dict]
 ):
-    on_begin_ingest_by_record(mock_koza_transform)
-    validate_transform_result(
-        result=transform_ingest_by_record(mock_koza_transform, test_record),
-        expected_nodes=result_nodes,
-        expected_edge=result_edge,
-        node_test_slots=NODE_TEST_SLOTS,
-        association_test_slots=ASSOCIATION_TEST_SLOTS
-    )
+
+    for result in transform_ingest_all_streaming(mock_koza_transform, test_record):
+        validate_transform_result(
+            result=result,
+            expected_nodes=result_nodes,
+            expected_edge=result_edge,
+            node_test_slots=NODE_TEST_SLOTS,
+            association_test_slots=ASSOCIATION_TEST_SLOTS
+        )
