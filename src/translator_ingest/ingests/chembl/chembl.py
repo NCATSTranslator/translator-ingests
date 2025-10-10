5233521f84631ba34ec5c59030ce09b67e743913
vdancik <vdancik@broadinstitute.org>
2025-10-10 15:08:41 -0400

    ChEMBL mechanism - binding ingest

--- src/translator_ingest/ingests/chembl/chembl.py
+++ src/translator_ingest/ingests/chembl/chembl.py
@@ -1,153 +1,185 @@
 import uuid
 import koza
-import pandas as pd
+import os
 from typing import Any, Iterable
+import tarfile
+import sqlite3
 
+import biolink_model.datamodel.pydanticmodel_v2 as bm
 from biolink_model.datamodel.pydanticmodel_v2 import (
     ChemicalEntity,
-    ChemicalToDiseaseOrPhenotypicFeatureAssociation,
-    Disease,
-    NamedThing,
     KnowledgeLevelEnum,
     AgentTypeEnum,
     Association
 )
 from koza.model.graphs import KnowledgeGraph
 
+LATEST_VERSION = "35"
 
-# !!! README First !!!
-#
-# This module provides a template with example code and instructions for implementing an ingest. Replace the body
-# of function examples below with ingest specific code and delete all template comments or unused functions.
-#
-# Note about ingest tags: for ingests with multiple different input files and/or different transformation processes,
-# ingests can be divided into multiple sections using tags. Examples from this template are "ingest_by_record",
-# "ingest_all", and "transform_ingest_all_streaming". Tags should be declared as keys in the readers section of ingest
-# yaml files, then included with the (tag="tag_id") syntax as parameters in corresponding koza decorators.
+INFORES_CHEMBL = "infores:chembl"
+
+MOA_QUERY = """
+        SELECT
+            molecule_dictionary.chembl_id AS molecule_chembl_id,
+            molecule_dictionary.pref_name AS molecule_name,
+            drug_mechanism.mec_id,
+            drug_mechanism.molregno,
+            drug_mechanism.mechanism_of_action,
+            drug_mechanism.action_type,
+            drug_mechanism.direct_interaction,
+            drug_mechanism.mechanism_comment,
+            drug_mechanism.selectivity_comment,
+            target_dictionary.chembl_id AS target_chembl_id,
+            target_dictionary.pref_name AS target_name,
+            target_dictionary.target_type,
+            target_dictionary.tax_id AS target_organism_tax_id,
+            binding_sites.site_name,
+            drug_mechanism.binding_site_comment,
+            source.src_description AS source_description,
+            docs.pubmed_id,
+            docs.doi,
+            docs.chembl_id AS document_chembl_id,
+            variant_sequences.mutation,
+            variant_sequences.accession AS mutation_accession
+        FROM drug_mechanism
+        JOIN molecule_dictionary ON molecule_dictionary.molregno=drug_mechanism.molregno
+        JOIN target_dictionary ON target_dictionary.tid=drug_mechanism.tid
+        LEFT JOIN binding_sites ON binding_sites.site_id=drug_mechanism.site_id
+        LEFT JOIN compound_records ON compound_records.record_id=drug_mechanism.record_id
+        LEFT JOIN docs ON (docs.doc_id=compound_records.doc_id AND compound_records.doc_id!=-1)
+        LEFT JOIN source ON source.src_id=compound_records.src_id
+        LEFT JOIN variant_sequences ON variant_sequences.variant_id = drug_mechanism.variant_id and drug_mechanism.variant_id != -1
+    """
+
+WHERE_DIRECT_INTERACTION = " WHERE direct_interaction = '1'"
 
 
-# Biolink predicates and infores identifiers will eventually be imported from automatically updated modules,
-# but for now use hardcoded constants.
-BIOLINK_RELATED_TO = "biolink:related_to"
-INFORES_CTD = "infores:ctd"
+TARGET_CLASS_MAP = {
+    "ADMET": "Cell",
+    "CELL-LINE": "CellLine",
+    "CHIMERIC PROTEIN": "MacromolecularComplex",
+    "LIPID": "MolecularEntity",
+    "MACROMOLECULE": "MolecularEntity",
+    "METAL": "MolecularEntity",
+    "MOLECULAR": "MolecularEntity",
+    "NO TARGET": None,
+    "NON-MOLECULAR": "BiologicalEntity",
+    "NUCLEIC-ACID": "NucleicAcidEntity",
+    "OLIGOSACCHARIDE": "MolecularEntity",
+    "ORGANISM": "CellularOrganism",
+    "PROTEIN": "Protein",
+    "PHENOTYPE": "PhenotypicFeature",
+    "PROTEIN COMPLEX": "MacromolecularComplex",
+    "PROTEIN COMPLEX GROUP": "MacromolecularComplex",
+    "PROTEIN FAMILY": "ProteinFamily",
+    "PROTEIN NUCLEIC-ACID COMPLEX": "MacromolecularComplex",
+    "PROTEIN-PROTEIN INTERACTION": "MolecularActivity",
+    "SELECTIVITY GROUP": "ProteinFamily",
+    "SINGLE PROTEIN": "Protein",
+    "SMALL MOLECULE": "SmallMolecule",
+    "SUBCELLULAR": "CellularComponent",
+    "TISSUE": "AnatomicalEntity",
+    "UNCHECKED": None,
+    "UNDEFINED": None,
+    "UNKNOWN": None
+}
 
-# Always implement a function that returns a string representing the latest version of the source data.
-# Ideally, this is the version provided by the knowledge source, directly associated with a specific data download.
-# If a source does not implement versioning, we need to do it. For static datasets, assign a version string
-# corresponding to the current version. For sources that are updated regularly, use file modification dates if
-# possible, or the current date. Versions should (ideally) be sortable (ie YYYY-MM-DD) and should contain no spaces.
+CHEMBL_COMPOUND_PREFIX = "CHEMBL.COMPOUND:"
+CHEMBL_TARGET_PREFIX = "CHEMBL.TARGET:"
+TAX_ID_PREFIX = "NCBITaxon:"
+PUBMED_PREFIX = "PMID:"
+DOI_PREFIX = "DOI:"
+CHEMBL_DODCUMENT_PREFIX = "CHEMBL.DOCUMENT:"
+
+BIOLINK_DIRECTLY_INTERACTS_WITH = "biolink:directly_physically_interacts_with"
+
+
+# This function returns the latest version of the data source.
 def get_latest_version() -> str:
-    return "v1"
+    return LATEST_VERSION
 
-# Functions decorated with @koza.on_data_begin() or @koza.on_data_end() are optional.
-# If implemented they will be called at the beginning and/or end of the transform process.
-@koza.on_data_begin(tag="ingest_by_record")
-def on_begin_ingest_by_record(koza: koza.KozaTransform) -> None:
-    # koza.state is a dictionary that can be used for arbitrary data storage, persisting across an individual transform.
-    koza.state['example_counter'] = 0
 
-@koza.on_data_end(tag="ingest_by_record")
-def on_end_ingest_by_record(koza: koza.KozaTransform) -> None:
-    # for example koza.state could be used for logging
-    if koza.state['example_counter'] > 0:
-        koza.log(f'{koza.state['example_counter']} rows were discarded for having no publications.', level="INFO")
-
-# Functions decorated with @koza.prepare_data() are optional. They are called after on_data_begin but before transform.
-# They take an Iterable of dictionaries, typically representing the rows of a source data file, and return an Iterable
-# of dictionaries which will be the data passed to subsequent transform functions. This allows for operations like
-# nontrivial merging or transforming of complex source data on a source wide level, even if the transform will occur
-# with a per record transform function.
-@koza.prepare_data(tag="ingest_by_record")
+# The prepare function is responsible for any data download, decompression, or other preparation required
+# before transformation. It should yield dictionaries, each representing a single record to be transformed.
+@koza.prepare_data(tag="chembl_drug_mechanism_binding")
 def prepare(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]] | None:
 
-    # do pandas stuff
-    df = pd.DataFrame(data)
-    return df.dropna().drop_duplicates().to_dict(orient="records")
-
-    # do database stuff:
-    # import sqlite3
-    # con = sqlite3.connect("example.db")
-    # con.row_factory = sqlite3.Row
-    # cur = con.cursor()
-    # cur.execute("SELECT * FROM example_table")
-    # records = cursor.fetchall()
-    # for record in records:
-    #     yield record
-    # con.close()
-
-    # merge stuff in a custom way
-    # koza.state['nodes'] = defaultdict(dict)
-    # for record in data:
-    #    koza.state['nodes'][record['node_id']] # store some merged node properties or something like that
+    print("Preparing data...")
+    version = get_latest_version()
+    download_file = f'data/chembl/chembl_{version}_sqlite.tar.gz'
+    database_path = f'data/chembl/chembl_{version}/chembl_{version}_sqlite/chembl_{version}.db'
+    # uncompress tar.gz file
+    if not os.path.exists(database_path):
+        print(f"Extracting {download_file} to {database_path} ...")
+        with tarfile.open(download_file, "r:gz") as tar:
+            tar.extractall(path='data/chembl/')
+        print("Extraction complete.")
 
 
-# Ingests must implement a function decorated with @koza.transform() OR @koza.transform_record() (not both).
-# These functions should contain the core data transformation logic generating and returning KnowledgeGraph objects
-# with NamedThing (nodes) and Association (edges) from source data.
-#
-# The transform_record function takes the KozaTransform and a single record, a dictionary typically corresponding to a
-# row in a source data file, and returns a KnowledgeGraph with any number of nodes and/or edges.
-@koza.transform_record(tag="ingest_by_record")
-def transform_ingest_by_record(koza: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
+    con = sqlite3.connect(database_path)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(MOA_QUERY + WHERE_DIRECT_INTERACTION)
+    records = cur.fetchall()
+    for record in records:
+         yield record
+    con.close()
 
-    # here is an example of skipping a record based off of some condition
-    publications = [f"PMID:{p}" for p in record["PubMedIDs"].split("|")] if record["PubMedIDs"] else None
-    if not publications:
-        koza.state['example_counter'] += 1
+
+def build_target_node(id: str, name: str, category: str):
+    if category not in TARGET_CLASS_MAP or TARGET_CLASS_MAP[category] is None:
         return None
+    cls = getattr(bm, TARGET_CLASS_MAP[category])
+    return cls(id=CHEMBL_TARGET_PREFIX+id, name=name)
 
-    chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-    disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-    association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-        id=str(uuid.uuid4()),
-        subject=chemical.id,
-        predicate=BIOLINK_RELATED_TO,
-        object=disease.id,
-        publications=publications,
-        primary_knowledge_source=INFORES_CTD,
-        knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-        agent_type=AgentTypeEnum.manual_agent,
-    )
-    return KnowledgeGraph(nodes=[chemical, disease], edges=[association])
-
-# As an alternative to transform_record, functions decorated with @koza.transform() take a KozaTransform and an Iterable
-# of dictionaries, typically corresponding to all the rows in a source data file, and return an iterable of
-# KnowledgeGraph, each containing any number of nodes and/or edges. Any number of KnowledgeGraphs can be returned:
-# all at once, in batches, or using a generator for streaming.
-@koza.transform(tag="ingest_all")
-def transform_ingest_all(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
-    nodes: list[NamedThing] = []
-    edges: list[Association] = []
-    for record in data:
-        chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-        disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-        association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-            id=str(uuid.uuid4()),
-            subject=chemical.id,
-            predicate=BIOLINK_RELATED_TO,
-            object=disease.id,
-            primary_knowledge_source=INFORES_CTD,
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.manual_agent,
-        )
-        nodes.append(chemical)
-        nodes.append(disease)
-        edges.append(association)
-    return [KnowledgeGraph(nodes=nodes, edges=edges)]
 
 # Here is an example using a generator to stream results
-@koza.transform(tag="ingest_all_streaming")
+@koza.transform(tag="chembl_drug_mechanism_binding")
 def transform_ingest_all_streaming(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
     for record in data:
-        chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-        disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-        association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-            id=str(uuid.uuid4()),
-            subject=chemical.id,
-            predicate=BIOLINK_RELATED_TO,
-            object=disease.id,
-            primary_knowledge_source=INFORES_CTD,
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.manual_agent,
-        )
-        yield KnowledgeGraph(nodes=[chemical, disease], edges=[association])
+        #print(record)
+        nodes = []
+        edges = []
+        chemical = ChemicalEntity(id=CHEMBL_COMPOUND_PREFIX+record["molecule_chembl_id"], name=record["molecule_name"])
+        target = build_target_node(record["target_chembl_id"], record["target_name"], record["target_type"])
+        if target is not None:
+            nodes.append(chemical)
+            nodes.append(target)
+
+            # add qualifiers if available
+            mutation_qualifier = "mutated" if record["mutation"] is not None else None
+            species_context_qualifier = TAX_ID_PREFIX+str(record["target_organism_tax_id"]) if record["target_organism_tax_id"] is not None else None
+
+            # add publications if available
+            publications = []
+            if record["pubmed_id"] is not None:
+                publications.append(PUBMED_PREFIX+str(record["pubmed_id"]))
+            elif record["doi"] is not None:
+                publications.append(DOI_PREFIX+str(record["doi"]))
+            elif record["document_chembl_id"] is not None:
+                publications.append(CHEMBL_DODCUMENT_PREFIX+str(record["document_chembl_id"]))
+            if len(publications) == 0:
+                publications = None
+            
+            # Create association
+            association = Association(
+                id=str(uuid.uuid4()),
+                subject=chemical.id,
+                predicate=BIOLINK_DIRECTLY_INTERACTS_WITH,
+                object=target.id,
+                # species_context_qualifier = species_context_qualifier,
+                # object_form_or_variant_qualifier = mutation_qualifier,
+                primary_knowledge_source=INFORES_CHEMBL,
+                knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+                agent_type=AgentTypeEnum.manual_agent,
+                publications=publications,
+                # binding_site_name = record["site_name"],
+                # binding_site_comment = record["binding_site_comment"],
+                # mechanism_of_action_description = record["mechanism_of_action"],
+                # mechanism_of_action_comment = record["mechanism_comment"],
+                # selectivity_comment = record["selectivity_comment"],
+                # mutation = record["mutation"],
+                # mutation_accession = record["mutation_accession"]
+            )
+            edges=[association]
+        yield KnowledgeGraph(nodes=nodes, edges=edges)
