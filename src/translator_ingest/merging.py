c6d258c2f4c827fa04190f746e78ddc6d1a87753
Evan Morris <evandietzmorris@gmail.com>
2025-11-11 02:14:03 -0500

    adding graph-metadata for merged graph

--- src/translator_ingest/merging.py
+++ src/translator_ingest/merging.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 from orion.kgx_file_merger import KGXFileMerger
 from orion.kgxmodel import GraphSpec, GraphSource
+from orion.kgx_validation import validate_graph as generate_graph_summary
 from orion.merging import DiskGraphMerger
+
 
 from translator_ingest import INGESTS_DATA_PATH
 from translator_ingest.util.metadata import PipelineMetadata
@@ -16,7 +18,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def merge(graph_id: str, sources: list[str], overwrite: bool = False):
+def merge(graph_id: str, sources: list[str], overwrite: bool = False) -> str:
     """Use ORION to merge multiple sources together into a single KGX output."""
     logger.info(f"Merging {graph_id} with {sources}.")
     graph_spec_sources = []
@@ -51,8 +53,12 @@ def merge(graph_id: str, sources: list[str], overwrite: bool = False):
     output_dir = Path(INGESTS_DATA_PATH) / graph_id / graph_version
     nodes_output_file = output_dir / "nodes.jsonl"
     edges_output_file = output_dir / "edges.jsonl"
-    if (nodes_output_file.exists() and edges_output_file.exists()) and not overwrite:
-        logger.info(f"Graph {graph_id} ({graph_version}) already exists! Exiting...")
+    if nodes_output_file.exists() and edges_output_file.exists():
+        if not overwrite:
+            logger.info(f"Graph {graph_id} ({graph_version}) already exists! Exiting...")
+            return graph_version
+        else:
+            logger.info(f"Graph {graph_id} ({graph_version}) already exists! OVERWRITE mode enabled, overwriting...")
 
     output_dir.mkdir(parents=True, exist_ok=True)
     file_merger = KGXFileMerger(
@@ -71,16 +77,44 @@ def merge(graph_id: str, sources: list[str], overwrite: bool = False):
     if "merge_error" in merge_metadata:
         logger.error(f"Merging error occurred: {merge_metadata['merge_error']}")
     else:
-        metadata_output = output_dir / "merge_metadata.json"
+        metadata_output = output_dir / "merge-metadata.json"
         with open(metadata_output, "w") as metadata_file:
             metadata_file.write(json.dumps(merge_metadata, indent=4))
+
+    return graph_version
+
+def merge_graph_metadata(graph_id: str, graph_version: str, overwrite: bool = False):
+    merged_graph_dir = Path(INGESTS_DATA_PATH) / graph_id / graph_version
+    merged_graph_nodes = merged_graph_dir / "nodes.jsonl"
+    merged_graph_edges = merged_graph_dir / "edges.jsonl"
+    graph_metadata_file_path = merged_graph_dir / "graph-metadata.json"
+    if graph_metadata_file_path.exists():
+        if not overwrite:
+            logger.error(f"Graph metadata file already exists: {graph_metadata_file_path}. Exiting...")
+        else:
+            logger.info(f"Graph metadata file already exists: {graph_metadata_file_path}. "
+                        f"OVERWRITE mode enabled, overwriting...")
+            return
+
+    # TODO need to get the equivalent of the RIG source_info for this graph
+    graph_metadata = generate_graph_summary(
+        nodes_file_path=str(merged_graph_nodes),
+        edges_file_path=str(merged_graph_edges),
+        graph_id=graph_id,
+        graph_version=graph_version,
+        logger=logger,
+    )
+    with graph_metadata_file_path.open("w") as output_file:
+        output_file.write(json.dumps(graph_metadata, indent=2))
+
 
 
 @click.command()
 @click.argument("graph_id", required=True)
 @click.argument("sources", nargs=-1, required=True)
 def main(graph_id, sources):
-    merge(graph_id, sources=list(sources))
+    graph_version = merge(graph_id, sources=list(sources))
+    merge_graph_metadata(graph_id, graph_version)
 
 
 if __name__ == "__main__":
