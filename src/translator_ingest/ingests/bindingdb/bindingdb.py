94ad6839fb6612cb629293286140213c742e5c11
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2025-11-19 19:44:09 -0800

    Another rough-in iteration of BindingDb

--- src/translator_ingest/ingests/bindingdb/bindingdb.py
+++ src/translator_ingest/ingests/bindingdb/bindingdb.py
@@ -33,27 +33,27 @@ from koza.model.graphs import KnowledgeGraph
 # corresponding to the current version. For sources that are updated regularly, use file modification dates if
 # possible, or the current date. Versions should (ideally) be sortable (ie YYYY-MM-DD) and should contain no spaces.
 def get_latest_version() -> str:
-    return "v1"
+    return "202511"  # asking the BindingDB team how to get their latest versions
 
 
 # Functions decorated with @koza.on_data_begin() or @koza.on_data_end() are optional.
 # If implemented they will be called at the beginning and/or end of the transform process.
-@koza.on_data_begin(tag="ingest_by_record")
-def on_begin_ingest_by_record(koza: koza.KozaTransform) -> None:
-    # koza.state is a dictionary that can be used for arbitrary data storage, persisting across an individual transform.
-    koza.state["example_counter"] = 0
-
-    # koza.transform_metadata is a dictionary that can be used to save arbitrary metadata, the contents of  which will
-    # be copied to metadata output files. transform_metadata persists across all tagged transforms for a source.
-    koza.transform_metadata["ingest_by_record"] = {"rows_missing_publications": 0}
-
-
-@koza.on_data_end(tag="ingest_by_record")
-def on_end_ingest_by_record(koza: koza.KozaTransform) -> None:
-    # for example koza.state could be used for logging
-    if koza.state["example_counter"] > 0:
-        koza.log(f"{koza.state['example_counter']} rows were discarded for having no publications.", level="INFO")
-        koza.transform_metadata["ingest_by_record"]["rows_missing_publications"] = koza.state["example_counter"]
+# @koza.on_data_begin(tag="ingest_by_record")
+# def on_begin_ingest_by_record(koza: koza.KozaTransform) -> None:
+#     # koza.state is a dictionary that can be used for arbitrary data storage, persisting across an individual transform.
+#     koza.state["example_counter"] = 0
+#
+#     # koza.transform_metadata is a dictionary that can be used to save arbitrary metadata, the contents of  which will
+#     # be copied to metadata output files. transform_metadata persists across all tagged transforms for a source.
+#     koza.transform_metadata["ingest_by_record"] = {"rows_missing_publications": 0}
+#
+#
+# @koza.on_data_end(tag="ingest_by_record")
+# def on_end_ingest_by_record(koza: koza.KozaTransform) -> None:
+#     # for example koza.state could be used for logging
+#     if koza.state["example_counter"] > 0:
+#         koza.log(f"{koza.state['example_counter']} rows were discarded for having no publications.", level="INFO")
+#         koza.transform_metadata["ingest_by_record"]["rows_missing_publications"] = koza.state["example_counter"]
 
 
 # Functions decorated with @koza.prepare_data() are optional. They are called after on_data_begin but before transform.
@@ -61,28 +61,28 @@ def on_end_ingest_by_record(koza: koza.KozaTransform) -> None:
 # of dictionaries which will be the data passed to subsequent transform functions. This allows for operations like
 # nontrivial merging or transforming of complex source data on a source wide level, even if the transform will occur
 # with a per record transform function.
-@koza.prepare_data(tag="ingest_by_record")
-def prepare(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]] | None:
-
-    # do pandas stuff
-    df = pd.DataFrame(data)
-    return df.dropna().drop_duplicates().to_dict(orient="records")
-
-    # do database stuff:
-    # import sqlite3
-    # con = sqlite3.connect("example.db")
-    # con.row_factory = sqlite3.Row
-    # cur = con.cursor()
-    # cur.execute("SELECT * FROM example_table")
-    # records = cursor.fetchall()
-    # for record in records:
-    #     yield record
-    # con.close()
-
-    # merge stuff in a custom way
-    # koza.state['nodes'] = defaultdict(dict)
-    # for record in data:
-    #    koza.state['nodes'][record['node_id']] # store some merged node properties or something like that
+# @koza.prepare_data(tag="ingest_by_record")
+# def prepare(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[dict[str, Any]] | None:
+#
+#     # do pandas stuff
+#     df = pd.DataFrame(data)
+#     return df.dropna().drop_duplicates().to_dict(orient="records")
+#
+#     # do database stuff:
+#     # import sqlite3
+#     # con = sqlite3.connect("example.db")
+#     # con.row_factory = sqlite3.Row
+#     # cur = con.cursor()
+#     # cur.execute("SELECT * FROM example_table")
+#     # records = cursor.fetchall()
+#     # for record in records:
+#     #     yield record
+#     # con.close()
+#
+#     # merge stuff in a custom way
+#     # koza.state['nodes'] = defaultdict(dict)
+#     # for record in data:
+#     #    koza.state['nodes'][record['node_id']] # store some merged node properties or something like that
 
 
 # Ingests must implement a function decorated with @koza.transform() OR @koza.transform_record() (not both).
@@ -122,43 +122,43 @@ def transform_ingest_by_record(koza: koza.KozaTransform, record: dict[str, Any])
 # of dictionaries, typically corresponding to all the rows in a source data file, and return an iterable of
 # KnowledgeGraph, each containing any number of nodes and/or edges. Any number of KnowledgeGraphs can be returned:
 # all at once, in batches, or using a generator for streaming.
-@koza.transform(tag="ingest_all")
-def transform_ingest_all(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
-    nodes: list[NamedThing] = []
-    edges: list[Association] = []
-    for record in data:
-        chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-        disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-        association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-            id=str(uuid.uuid4()),
-            subject=chemical.id,
-            predicate="biolink:related_to",
-            object=disease.id,
-            primary_knowledge_source=INFORES_CTD,
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.manual_agent,
-        )
-        nodes.append(chemical)
-        nodes.append(disease)
-        edges.append(association)
-    return [KnowledgeGraph(nodes=nodes, edges=edges)]
+# @koza.transform(tag="ingest_all")
+# def transform_ingest_all(koza: koza.KozaTransform, data: Iterable[dict[str, Any]]) -> Iterable[KnowledgeGraph]:
+#     nodes: list[NamedThing] = []
+#     edges: list[Association] = []
+#     for record in data:
+#         chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
+#         disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
+#         association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
+#             id=str(uuid.uuid4()),
+#             subject=chemical.id,
+#             predicate="biolink:related_to",
+#             object=disease.id,
+#             primary_knowledge_source=INFORES_CTD,
+#             knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+#             agent_type=AgentTypeEnum.manual_agent,
+#         )
+#         nodes.append(chemical)
+#         nodes.append(disease)
+#         edges.append(association)
+#     return [KnowledgeGraph(nodes=nodes, edges=edges)]
 
 
 # Here is an example using a generator to stream results
-@koza.transform(tag="ingest_all_streaming")
-def transform_ingest_all_streaming(
-    koza: koza.KozaTransform, data: Iterable[dict[str, Any]]
-) -> Iterable[KnowledgeGraph]:
-    for record in data:
-        chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
-        disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
-        association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
-            id=str(uuid.uuid4()),
-            subject=chemical.id,
-            predicate="biolink:related_to",
-            object=disease.id,
-            primary_knowledge_source=INFORES_CTD,
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.manual_agent,
-        )
-        yield KnowledgeGraph(nodes=[chemical, disease], edges=[association])
+# @koza.transform(tag="ingest_all_streaming")
+# def transform_ingest_all_streaming(
+#     koza: koza.KozaTransform, data: Iterable[dict[str, Any]]
+# ) -> Iterable[KnowledgeGraph]:
+#     for record in data:
+#         chemical = ChemicalEntity(id="MESH:" + record["ChemicalID"], name=record["ChemicalName"])
+#         disease = Disease(id=record["DiseaseID"], name=record["DiseaseName"])
+#         association = ChemicalToDiseaseOrPhenotypicFeatureAssociation(
+#             id=str(uuid.uuid4()),
+#             subject=chemical.id,
+#             predicate="biolink:related_to",
+#             object=disease.id,
+#             primary_knowledge_source=INFORES_CTD,
+#             knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+#             agent_type=AgentTypeEnum.manual_agent,
+#         )
+#         yield KnowledgeGraph(nodes=[chemical, disease], edges=[association])
