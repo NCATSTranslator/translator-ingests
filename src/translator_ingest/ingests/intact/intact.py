74a8d6a39a780254edfd00e30376401ee4dad8e9
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-11-26 15:13:03 -0800

    fix yaml formatting issues

--- src/translator_ingest/ingests/intact/intact.py
+++ src/translator_ingest/ingests/intact/intact.py
@@ -11,7 +11,8 @@ from biolink_model.datamodel.pydanticmodel_v2 import (
     KnowledgeLevelEnum,
     AgentTypeEnum,
 )
-from translator_ingest.util.biolink import INFORES_INTACT, entity_id, build_association_knowledge_sources
+from translator_ingest.util.biolink import INFORES_INTACT
+from bmt.pydantic import entity_id, build_association_knowledge_sources
 from koza.model.graphs import KnowledgeGraph
 
 # PSI-MI interaction type to Biolink predicate mapping
