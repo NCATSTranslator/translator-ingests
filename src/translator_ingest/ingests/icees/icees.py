55b586c48c4863f4a7b2b0df1163e21d618faa55
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2026-02-03 14:33:00 -0800

    Interim commit - removed try blocks and extra on_data* calls (superfluous) but unit tests not revised and checked (yet)

--- src/translator_ingest/ingests/icees/icees.py
+++ src/translator_ingest/ingests/icees/icees.py
@@ -27,149 +27,96 @@ def get_latest_version() -> str:
     return "2024-08-20"  # last Phase 2 release of ICEES
 
 
-@koza.on_data_begin(tag="icees_nodes")
-def on_begin_node_ingest(koza_transform: koza.KozaTransform) -> None:
-    koza_transform.log("Starting ICEES nodes transformation")
-    koza_transform.transform_metadata["icees_nodes"] = {}
-
-
-@koza.on_data_end(tag="icees_nodes")
-def on_end_node_ingest(koza_transform: koza.KozaTransform) -> None:
-    if koza_transform.transform_metadata["icees_nodes"]:
-        for tag, value in koza_transform.transform_metadata["icees_nodes"].items():
-            koza_transform.log(
-                msg=f"Exception {str(tag)} encountered for records: {',\n'.join(value)}.",
-                level="WARNING"
-            )
-    koza_transform.log("End of ICEES nodes transformation")
-
 @koza.transform_record(tag="icees_nodes")
 def transform_icees_node(
         koza_transform: koza.KozaTransform,
         record: dict[str, Any]
 ) -> KnowledgeGraph | None:
+    node_id = record["id"]
 
-    try:
-        node_id = record["id"]
-
-        # the node 'category' may be a list of ancestor types
-        # along with the most specific type, but the Pydantic
-        # class returned is only of the most specific type.
-        category = record.get("category", [])
-        node_class = get_node_class(node_id, category, bmt=bmt)
-        if node_class is None:
-            logger.warning(f"Pydantic class for node '{node_id}' could not be created for category '{category}'")
-            return None
-
-        equivalent_identifiers: Optional[list[str]] = record.get("equivalent_identifiers", None)
-
-        node = node_class(
-            id=node_id,
-            name=record["name"],
-            equivalent_identifiers=equivalent_identifiers,
-            **{}
-        )
-        return KnowledgeGraph(nodes=[node])
-
-    except Exception as e:
-        # Tally errors here
-        exception_tag = f"{str(type(e))}: {str(e)}"
-        rec_id = record.get("id", "Unknown")
-        if exception_tag not in koza_transform.transform_metadata["icees_nodes"]:
-            koza_transform.transform_metadata["icees_nodes"][exception_tag] = [rec_id]
-        else:
-            koza_transform.transform_metadata["icees_nodes"][exception_tag].append(rec_id)
-
+    # the node 'category' may be a list of ancestor types
+    # along with the most specific type, but the Pydantic
+    # class returned is only of the most specific type.
+    category = record.get("category", [])
+    node_class = get_node_class(node_id, category, bmt=bmt)
+    if node_class is None:
+        logger.warning(f"Pydantic class for node '{node_id}' could not be created for category '{category}'")
         return None
 
+    equivalent_identifiers: Optional[list[str]] = record.get("equivalent_identifiers", None)
 
-@koza.on_data_begin(tag="icees_edges")
-def on_begin_edge_ingest(koza_transform: koza.KozaTransform) -> None:
-    koza_transform.log("Starting ICEES edges transformation")
-    koza_transform.log(f"Version: {get_latest_version()}")
-    koza_transform.transform_metadata["icees_edges"] = {}
+    node = node_class(
+        id=node_id,
+        name=record["name"],
+        equivalent_identifiers=equivalent_identifiers,
+        **{}
+    )
+    return KnowledgeGraph(nodes=[node])
 
-
-@koza.on_data_end(tag="icees_edges")
-def on_end_edge_ingest(koza_transform: koza.KozaTransform) -> None:
-    if koza_transform.transform_metadata["icees_edges"]:
-        for tag, value in koza_transform.transform_metadata["icees_edges"].items():
-            koza_transform.log(
-                msg=f"Exception {str(tag)} encountered for records: {',\n'.join(value)}.",
-                level="WARNING"
-            )
-    koza_transform.log("End of ICEES edges transformation")
 
 @koza.transform_record(tag="icees_edges")
 def transform_icees_edge(koza_transform: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
+    """
 
-    try:
-        edge_id = entity_id()
+    :param koza_transform:
+    :param record:
+    :return:
+    """
+    edge_id = entity_id()
 
-        icees_subject: str = record["subject"]
-        subject_category: list[str] = bmt.get_element_by_prefix(icees_subject)
+    icees_subject: str = record["subject"]
+    subject_category: list[str] = bmt.get_element_by_prefix(icees_subject)
 
-        icees_predicate: str = record["predicate"]
+    icees_predicate: str = record["predicate"]
 
-        icees_object: str = record["object"]
-        object_category: list[str] = bmt.get_element_by_prefix(icees_object)
-        association_list = bmt.get_associations(
-                    subject_categories=subject_category,
-                    predicates= [icees_predicate],
-                    object_categories=object_category,
-                    formatted=True
-            )
-
-        edge_class = get_edge_class(edge_id, associations=association_list, bmt=bmt)
-
-        remapped_predicate: str
-        negation: bool
-        remapped_predicate, negation = remap_icees_predicate(
-            association_type=edge_class.__name__,
-            predicate=icees_predicate
+    icees_object: str = record["object"]
+    object_category: list[str] = bmt.get_element_by_prefix(icees_object)
+    association_list = bmt.get_associations(
+                subject_categories=subject_category,
+                predicates= [icees_predicate],
+                object_categories=object_category,
+                formatted=True
         )
 
-        # Convert many of the ICEES edge attributes into specific edge properties
-        supporting_studies: dict[str, Study] = {}
-        icees_qualifiers: dict[str,str] = {}
-        attributes = record["attributes"]
-        for attribute_string in attributes:
-            # is 'attribute' a dict, or string serialized version of a dict?
-            attribute_data = json.loads(attribute_string)
-            if attribute_data["attribute_type_id"] == "icees_cohort_identifier":
-                study_id = attribute_data["value"]
-                supporting_studies[study_id] = get_icees_supporting_study(
-                                                    edge_id=edge_id,
-                                                    study_id=study_id,
-                                                    result=attribute_data["attributes"]
-                                                )
-            elif attribute_data["attribute_type_id"] in ["subject_feature_name","object_feature_name"]:
-                icees_qualifiers[attribute_data["attribute_type_id"]] = attribute_data["value"]
-            else:
-                pass # all other attributes ignored at this time
+    edge_class = get_edge_class(edge_id, associations=association_list, bmt=bmt)
 
-        association = edge_class(
-            id=entity_id(),
-            subject=icees_subject,
-            predicate=remapped_predicate,
-            negated=negation,
-            object=icees_object,
-            has_supporting_studies=supporting_studies,
-            sources=build_association_knowledge_sources(primary=record["primary_knowledge_source"]),
-            knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
-            agent_type=AgentTypeEnum.not_provided,
-            **icees_qualifiers
-        )
+    remapped_predicate: str
+    negation: bool
+    remapped_predicate, negation = remap_icees_predicate(
+        association_type=edge_class.__name__,
+        predicate=icees_predicate
+    )
 
-        return KnowledgeGraph(edges=[association])
-
-    except Exception as e:
-        # Tally errors here
-        exception_tag = f"{str(type(e))}: {str(e)}"
-        rec_id = record.get("id", "Unknown")
-        if exception_tag not in koza_transform.transform_metadata["icees_edges"]:
-            koza_transform.transform_metadata["icees_edges"][exception_tag] = [rec_id]
+    # Convert many of the ICEES edge attributes into specific edge properties
+    supporting_studies: dict[str, Study] = {}
+    icees_qualifiers: dict[str,str] = {}
+    attributes = record["attributes"]
+    for attribute_string in attributes:
+        # is 'attribute' a dict, or string serialized version of a dict?
+        attribute_data = json.loads(attribute_string)
+        if attribute_data["attribute_type_id"] == "icees_cohort_identifier":
+            study_id = attribute_data["value"]
+            supporting_studies[study_id] = get_icees_supporting_study(
+                                                edge_id=edge_id,
+                                                study_id=study_id,
+                                                result=attribute_data["attributes"]
+                                            )
+        elif attribute_data["attribute_type_id"] in ["subject_feature_name","object_feature_name"]:
+            icees_qualifiers[attribute_data["attribute_type_id"]] = attribute_data["value"]
         else:
-            koza_transform.transform_metadata["icees_edges"][exception_tag].append(rec_id)
+            pass # all other attributes ignored at this time
 
-        return None
+    association = edge_class(
+        id=entity_id(),
+        subject=icees_subject,
+        predicate=remapped_predicate,
+        negated=negation,
+        object=icees_object,
+        has_supporting_studies=supporting_studies,
+        sources=build_association_knowledge_sources(primary=record["primary_knowledge_source"]),
+        knowledge_level=KnowledgeLevelEnum.knowledge_assertion,
+        agent_type=AgentTypeEnum.not_provided,
+        **icees_qualifiers
+    )
+
+    return KnowledgeGraph(edges=[association])
