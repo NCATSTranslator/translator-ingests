9e0118f48ee878ac511646f12323d930c30983f9
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2026-02-19 14:45:26 -0800

    Update panther_orthologs_utils.py
    Removed superseded panther_orthologs_utils.parse_gene_info

--- src/translator_ingest/ingests/panther/panther_orthologs_utils.py
+++ src/translator_ingest/ingests/panther/panther_orthologs_utils.py
@@ -84,67 +84,10 @@ RAW_COLUMNS = [GENE_COL, ORTHOLOG_COL, TYPE_OF_ORTHOLOG_COL, COMMON_ANCESTOR_COL
 TARGET_SPECIES = set(panther_taxon_map.keys())
 
 
-def parse_gene_info(
-        gene_info,
-        taxon_map,
-        curie_map,
-        # fallback_map - we don't use this NCBI Gene lookup at the moment, but we keep it here for now for reference'
-):
-    """
-    This function takes a panther gene information string and returns the species name and gene identifier in a
-    standardized format. This is done by converting to CURIEs based on a predefined mapping in a table and using
-    uniprotkb id as a fallback. We also remove ensemble version/transcript ids from the tail end of ensembl ids,
-    and we also filter out species that are not in our taxon map. Below are examples of the transformation process
-
-    HUMAN|HGNC=16435|UniProtKB=Q8TCT9 --> HUMAN, HGNC:16435
-    SCHPO|PomBase=SPBP23A10.09|UniProtKB=Q9P7X6 --> SCHPO, PomBase:SPBP23A10.09
-    CANLF|Ensembl=ENSCAFG00845009646.1|UniProtKB=A0A8I3N1X7 --> CANLF, Ensembl:ENSCAFG00845009646
-
-    :param gene_info: This is a string of the format species|gene|uniprotkb_id
-    :param taxon_map: This is a dictionary of the Panther standard species name to NCBI taxon id
-    :param curie_map: This is a dictionary of the gene  CURIE prefix mappings
-    :return:
-    """
-    cols = gene_info.split("|") # species|gene|uniprotkb_id
-    species = cols[0]
-
-    # Exit condition (saves compute when there are many rows to process...)
-    if species not in taxon_map:
-        return None, None
-
-    # Now assign our gene to its "rightful" prefix...
-    # If no reasonable prefix exists (HGNC, MGI, etc.),
-    # then we use the UniprotKB ID prefix as a fallback.
-    # Connections can be rescued through
-    # a normalization process, via UniProtKB protein ids
-
-    # Our preferred order is Organism specific (HGNC, PomBase, ZFIN)
-    gene_split = cols[1].split("=")
-
-    # Check if gene id can be mapped directly to kg build preferred gene ids
-    if gene_split[0] in curie_map:
-        # We use -1 here to avoid things like MGI=MGI=95886
-        gene = "{}:{}".format(curie_map[gene_split[0]], gene_split[-1])
-        # matched = 1
-
-    # Use the UniProtKB id as a last resort and
-    # format e.g., UniProtKB=Q8TCT9 => "UniProtKB:Q8TCT9"
-    else:
-        gene = "{}".format(cols[-1].replace("=", ":"))
-        # unikb += 1
-
-    # Lastly we need to strip version numbers off from ENSEMBL IDs,
-    # (e.g. ENSG00000123456.1 => ENSG00000123456)
-    if gene.startswith("ENSEMBL:") and (":ENS" in gene):
-        gene = gene.split(".")[0]
-
-    return species, gene,
-
-
 def _resolve_gene_curie(col_name: str) -> pl.Expr:
     """
-    Build a polars expression that replicates parse_gene_info CURIE resolution logic
-    for a given column (Gene or Ortholog).
+    Build a polars expression that supersedes former parse_gene_info() utility method
+    by implementing CURIE resolution logic for a given polars column (Gene or Ortholog).
 
     The input column value is formatted as: ``species|DB=id|UniProtKB=uniprotid``
 
