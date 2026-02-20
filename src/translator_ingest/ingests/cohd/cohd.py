df358dc0470cb4dcd85441b895c1293c984a3576
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2026-02-19 20:40:42 -0800

    Remove "try..except" blocks (will provide other validation tests in a later commit)

--- src/translator_ingest/ingests/cohd/cohd.py
+++ src/translator_ingest/ingests/cohd/cohd.py
@@ -37,88 +37,64 @@ def transform_cohd_node(
         record: dict[str, Any]
 ) -> KnowledgeGraph | None:
 
-    try:
-        # COHD uses the value of a "categories" field to indicate the type of node
-        # We use it here to specify the correct Pydantic node class model
-        node_id = record["id"]
-        node_class = get_node_class(node_id, record.get("categories", ["biolink:NamedThing"]), bmt=bmt)
+    # COHD uses the value of a "categories" field to indicate the type of node
+    # We use it here to specify the correct Pydantic node class model
+    node_id = record["id"]
+    node_class = get_node_class(node_id, record.get("categories", ["biolink:NamedThing"]), bmt=bmt)
 
-        # TODO: need to figure out how to handle (certain?) attributes
-        # attributes = record["attributes"]
-        # # dct:description, biolink:same_as (equivalent_identifiers), etc.
-        # for attribute in attributes:
-        #     node.attributes.append(
-        #         Attribute(
-        #             attribute_type_id=attribute["attribute_type_id"],
-        #             value=attribute["value"]
-        #         )
-        #     )
+    # TODO: need to figure out how to handle (certain?) attributes
+    # attributes = record["attributes"]
+    # # dct:description, biolink:same_as (equivalent_identifiers), etc.
+    # for attribute in attributes:
+    #     node.attributes.append(
+    #         Attribute(
+    #             attribute_type_id=attribute["attribute_type_id"],
+    #             value=attribute["value"]
+    #         )
+    #     )
 
-        node = node_class(id=node_id, name=record["name"], **{})
-        return KnowledgeGraph(nodes=[node])
-
-    except Exception as e:
-        # Tally errors here
-        exception_tag = f"{str(type(e))}: {str(e)}"
-        rec_id = record.get("id", "Unknown")
-        if exception_tag not in koza_transform.transform_metadata["cohd_nodes"]:
-            koza_transform.transform_metadata["cohd_nodes"][exception_tag] = [rec_id]
-        else:
-            koza_transform.transform_metadata["cohd_nodes"][exception_tag].append(rec_id)
-
-        return None
+    node = node_class(id=node_id, name=record["name"], **{})
+    return KnowledgeGraph(nodes=[node])
 
 
 @koza.transform_record(tag="cohd_edges")
 def transform_cohd_edge(koza_transform: koza.KozaTransform, record: dict[str, Any]) -> KnowledgeGraph | None:
 
-    try:
-        edge_id = entity_id()
+    edge_id = entity_id()
 
-        cohd_subject: str = record["subject"]
-        # subject_category: list[str] = bmt.get_element_by_prefix(cohd_subject)
+    cohd_subject: str = record["subject"]
+    # subject_category: list[str] = bmt.get_element_by_prefix(cohd_subject)
 
-        cohd_predicate: str = record["predicate"]
+    cohd_predicate: str = record["predicate"]
 
-        cohd_object: str = record["object"]
-        # object_category: list[str] = bmt.get_element_by_prefix(cohd_object)
+    cohd_object: str = record["object"]
+    # object_category: list[str] = bmt.get_element_by_prefix(cohd_object)
 
-        # TODO: need to figure out how to handle (certain?) attributes
-        # attributes = parse_attributes(record.get("attributes", None))
-        #
-        # TODO: it would also be nice to have dynamic mapping of edge classes for COHD data;
-        #       However, for now, existing observations suggest that the Biolink model
-        #       requires a bit of review and revision to better support such dynamic mapping.
-        #
-        # association_list = bmt.get_associations(
-        #             subject_categories=subject_category,
-        #             predicates= [cohd_predicate],
-        #             object_categories=object_category,
-        #             formatted=True
-        #     )
-        #
-        # edge_class = get_edge_class(edge_id, associations=association_list, bmt=bmt)
-        #
-        # association = edge_class(
-        association = Association(
-            id=edge_id,
-            subject=cohd_subject,
-            predicate=cohd_predicate,
-            object=cohd_object,
-            has_confidence_score=record.get("score", None),
-            sources=knowledge_sources_from_trapi(record["sources"]),
-            knowledge_level=KnowledgeLevelEnum.statistical_association,
-            agent_type=AgentTypeEnum.data_analysis_pipeline,
-        )
-        return KnowledgeGraph(edges=[association])
-
-    except Exception as e:
-        # Tally errors here
-        exception_tag = f"{str(type(e))}: {str(e)}"
-        rec_id = record.get("id", "Unknown")
-        if exception_tag not in koza_transform.transform_metadata["cohd_edges"]:
-            koza_transform.transform_metadata["cohd_edges"][exception_tag] = [rec_id]
-        else:
-            koza_transform.transform_metadata["cohd_edges"][exception_tag].append(rec_id)
-
-        return None
+    # TODO: need to figure out how to handle (certain?) attributes
+    # attributes = parse_attributes(record.get("attributes", None))
+    #
+    # TODO: it would also be nice to have dynamic mapping of edge classes for COHD data;
+    #       However, for now, existing observations suggest that the Biolink model
+    #       requires a bit of review and revision to better support such dynamic mapping.
+    #
+    # association_list = bmt.get_associations(
+    #             subject_categories=subject_category,
+    #             predicates= [cohd_predicate],
+    #             object_categories=object_category,
+    #             formatted=True
+    #     )
+    #
+    # edge_class = get_edge_class(edge_id, associations=association_list, bmt=bmt)
+    #
+    # association = edge_class(
+    association = Association(
+        id=edge_id,
+        subject=cohd_subject,
+        predicate=cohd_predicate,
+        object=cohd_object,
+        has_confidence_score=record.get("score", None),
+        sources=knowledge_sources_from_trapi(record["sources"]),
+        knowledge_level=KnowledgeLevelEnum.statistical_association,
+        agent_type=AgentTypeEnum.data_analysis_pipeline,
+    )
+    return KnowledgeGraph(edges=[association])
