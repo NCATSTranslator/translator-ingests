e69c5148e0e61b31773e90b63bc77fca07642358
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2026-08-19 13:23:28 -0700

    Initial release of ontology lookup utilities

--- src/translator_ingest/util/ontology.py
+++ src/translator_ingest/util/ontology.py
@@ -1,31 +1,45 @@
 """
-A module of utilities for accessing public ontology terms.
+A module of utilities for accessing public ontology terms
+using the EBI Ontology Lookup Service (OLS)
 """
+
+# TODO: this initial implementation is primarily exact matching
+#       (with some ad hoc (hard coded) map normalization of some outlier names)
+#       However, some use cases may benefit from inexact matches and scoring of hits.
 
 from functools import lru_cache
 import requests
 
 OLS_SEARCH = "https://www.ebi.ac.uk/ols4/api/search"
 
-UBERON_NORMALIZATION = {
-    "csf": "cerebrospinal fluid",
-    "cerebrospinal fluid (csf)": "cerebrospinal fluid",
-    "faeces": "feces"
+_QUERY_REMAP = {
+    "go": {},
+    "mondo": {},
+    "uberon":
+    {
+        "csf": "cerebrospinal fluid",
+        "cerebrospinal fluid (csf)": "cerebrospinal fluid",
+        "faeces": "feces"
+    }
 }
 
-def _normalize_uberon_query(query):
-    return UBERON_NORMALIZATION.get(query.lower(), query)
+def _remap(query: str, ontology: str)->str:
+    # This method remaps some encountered alias names
+    # to more canonical terms for a given ontology
+    mappings = _QUERY_REMAP.get(ontology.lower(), {})
+    return mappings.get(query.lower(), query)
 
-# Not sure how large a cache is appropriate here,
-# but it's likely a small set of terms accessed per run
-@lru_cache
-def lookup_uberon(query: str):
 
-    normalized_query = _normalize_uberon_query(query)
+@lru_cache(maxsize=10000)
+def lookup(query: str, ontology: str | None = None)->dict[str,str] | None:
+
+    assert ontology is not None, "Ontology must be specified"
+
+    normalized_query = _remap(query, ontology)
 
     params = {
         "q": normalized_query,
-        "ontology": "uberon",
+        "ontology": ontology,
         "exact": "true"
     }
 
@@ -40,9 +54,30 @@ def lookup_uberon(query: str):
     best = docs[0]
 
     return {
+        "ontology": ontology,
         "input": query,
         "normalization": normalized_query,
         "label": best.get("label"),
-        "uberon_id": best.get("obo_id"),
+        "id": best.get("obo_id"),
         "iri": best.get("iri")
     }
+
+# Not sure how large the caches should be here, but
+# there are likely only a modest set of terms accessed per run
+
+@lru_cache(maxsize=None)
+def lookup_go(query: str)->dict[str,str] | None:
+    """
+    Gene Ontology (GO) name to term lookup
+    """
+    return lookup(query, "go")
+
+
+@lru_cache(maxsize=None)
+def lookup_mondo(query: str)->dict[str,str] | None:
+    return lookup(query, "mondo")
+
+
+@lru_cache(maxsize=None)
+def lookup_uberon(query: str)->dict[str,str] | None:
+    return lookup(query, "uberon")
--- tests/test_ontology.py
+++ tests/test_ontology.py
@@ -1,6 +1,49 @@
 import pytest
 
-from translator_ingest.util.ontology import lookup_uberon
+from translator_ingest.util.ontology import lookup, lookup_go, lookup_mondo, lookup_uberon
+
+@pytest.mark.parametrize(
+    "name,ontology,expected",
+    [
+        ("Glucose", "CHEBI","CHEBI:17234"),
+    ],
+)
+def test_generic_lookup(name:str, ontology:str, expected:str):
+    entry: dict = lookup(name,ontology)
+    assert entry["id"] == expected,\
+        f"Entry '{entry!s}' returned for query name: '{name}' did not match expected term '{expected}'"
+
+
+@pytest.mark.parametrize(
+    "name,expected",
+    [
+        ("Cytoplasm", "GO:0005737"),
+        ("cell proliferation","GO:1904898"),
+        ("apoptotic process","GO:0006915"),
+        ("mitochondrion","GO:0005739"),
+        ("DNA binding","GO:0003677")
+    ],
+)
+def test_go_lookup(name:str, expected:str):
+    entry: dict = lookup_go(name)
+    assert entry["id"] == expected,\
+        f"Entry '{entry!s}' returned for query name: '{name}' did not match expected term '{expected}'"
+
+
+@pytest.mark.parametrize(
+    "name,expected",
+    [
+        ("Werner Syndrome", "MONDO:0010196"),
+        ("breast carcinoma","MONDO:0004989"),
+        ("breast cancer","MONDO:0007254"),
+        ("Von Hippel-Lindau","MONDO:0008667")
+    ],
+)
+def test_mondo_lookup(name:str, expected:str):
+    entry: dict = lookup_mondo(name)
+    assert entry["id"] == expected,\
+        f"Entry '{entry!s}' returned for query name: '{name}' did not match expected term '{expected}'"
+
 
 @pytest.mark.parametrize(
     "name,expected",
@@ -21,9 +64,10 @@ from translator_ingest.util.ontology import lookup_uberon
 
         # Gross anatomical structures
         ("Placenta", "UBERON:0001987"),
-        ("Skeletal muscle", "UBERON:0004857"),
+        ("Skeletal muscle", "UBERON:0004857")
     ],
 )
 def test_uberon_lookup(name:str, expected:str):
     entry: dict = lookup_uberon(name)
-    assert entry["uberon_id"] == expected, f"Entry '{entry!s}' returned for query name: '{name}'"
+    assert entry["id"] == expected,\
+        f"Entry '{entry!s}' returned for query name: '{name}' did not match expected term '{expected}'"
